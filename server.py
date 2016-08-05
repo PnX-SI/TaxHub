d6c7a476828f1a86b59fce3a8861def454018ade
amandine-sahl <amandine.sahl@gmail.com>
2016-08-05 17:32:39 +0200

    Ralonger la validité du token à chaque action

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 #coding: utf8
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import importlib
+import datetime
+
 db = SQLAlchemy()
 app_globals = {}
 
 def init_app():
     if app_globals.get('app', False):
-        app= app_globals['app']
+        app = app_globals['app']
     else :
         app = Flask(__name__)
 
@@ -38,7 +40,21 @@ def init_app():
 
     from apptax.taxonomie.routesbibtypesmedia import adresses
     app.register_blueprint(adresses, url_prefix='/api/bibtypesmedia')
+
+    @app.after_request
+    def after_request(response):
+        try:
+            print('after_request')
+            if 'token' in request.cookies:
+                cookie_exp = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds= app.config['COOKIE_EXPIRATION'])
+                response.set_cookie('token', request.cookies['token'], expires=cookie_exp)
+                response.set_cookie('currentUser', request.cookies['currentUser'], expires=cookie_exp)
+            return response
+        except Exception as e:
+            print ('NO TOKEN')
+            return response
+
     return app
 
 if __name__ == '__main__':
-    init_app().run(debug=True)
+    init_app().run()
