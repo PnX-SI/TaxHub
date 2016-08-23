f6bc0a96c39200b544cfea9a270cd86046205880
amandine-sahl <amandine.sahl@gmail.com>
2016-08-23 15:38:01 +0200

    Manipulation des fichiers suite au commit en base de façon à traiter le rollback

--- apptax/taxonomie/routestmedias.py
+++ apptax/taxonomie/routestmedias.py
@@ -64,73 +64,66 @@ def get_tmediasbyType(type):
 @adresses.route('/<int:id_media>', methods=['POST', 'PUT'])
 @fnauth.check_auth(4)
 def insertUpdate_bibtaxons(id_media=None):
-    print ('insertUpdate_bibtaxons')
-
-    if request.files :
-        file = request.files['file']
-    data = {}
-    if request.form :
-        formData = dict(request.form)
-        for key in formData :
-            data[key] = formData[key][0]
-    else :
-        data = request.get_json(silent=True)
-    print(data)
-    #Si MAJ : récupération des données; Sinon création d'un nouvel objet
-    if id_media:
-        myMedia = db.session.query(TMedias).filter_by(id_media=id_media).first()
-        myMedia.cd_ref = data['cd_ref']
-    else:
-        myMedia = TMedias(cd_ref =  int(data['cd_ref']))
-
-    #Si il y a un fichier et qu'il est différent du précédent
-    if ('file' in locals()) and ((data['isFile'] == True) or (data['isFile'] == 'true' )):
-        print('chemin')
-        myMedia.url = ''
-        if myMedia.chemin != '' :
-            remove_file(myMedia.chemin)
-
-        filepath = upload_file(file, myMedia.cd_ref, data['titre'])
-        myMedia.chemin = filepath
-    elif ('url' in data) and (data['url'] != 'null') and (data['isFile'] != True) :
-        print('url')
-        myMedia.url = data['url']
-        if myMedia.chemin != '' :
-            remove_file(myMedia.chemin)
-            myMedia.chemin = ''
-
-    myMedia.titre = data['titre']
-    if 'auteur' in data :
-        myMedia.auteur = data['auteur']
-    if 'desc_media' in data :
-        myMedia.desc_media = data['desc_media']
-    # date_media = data['date_media'], TODO : voir le mode de gestion de la date du media (trigger ???)
-    myMedia.is_public = data['is_public']
-    myMedia.supprime = "false"
-    myMedia.id_type = data['id_type']
-
-    db.session.add(myMedia)
     try :
-        db.session.commit()
-    except IntegrityError as e:
-        db.session.rollback()
-
-        if re.match('.*is_unique_titre.*', repr(e.args)) is not None:
-            message = 'Le titre du média doit être unique'
+        if request.files :
+            file = request.files['file']
+        data = {}
+        if request.form :
+            formData = dict(request.form)
+            for key in formData :
+                data[key] = formData[key][0]
         else :
-            message = repr(e.args)
+            data = request.get_json(silent=True)
 
-        return json.dumps({'success':False, 'message': message }), 500, {'ContentType':'application/json'}
+        #Si MAJ : récupération des données; Sinon création d'un nouvel objet
+        if id_media:
+            myMedia = db.session.query(TMedias).filter_by(id_media=id_media).first()
+            myMedia.cd_ref = data['cd_ref']
+        else:
+            myMedia = TMedias(cd_ref =  int(data['cd_ref']))
+
+        myMedia.titre = data['titre']
+        if 'auteur' in data :
+            myMedia.auteur = data['auteur']
+        if 'desc_media' in data :
+            myMedia.desc_media = data['desc_media']
+        # date_media = data['date_media'], TODO : voir le mode de gestion de la date du media (trigger ???)
+        myMedia.is_public = data['is_public']
+        myMedia.supprime = "false"
+        myMedia.id_type = data['id_type']
+
+        db.session.add(myMedia)
+        try :
+            db.session.commit()
+        except IntegrityError as e:
+            db.session.rollback()
+            return json.dumps({'success':False, 'message': repr(e.args) }), 500, {'ContentType':'application/json'}
+        except Exception as e:
+            db.session.rollback()
+            return json.dumps({'success':False }), 500, {'ContentType':'application/json'}
+
+
+        if ('file' in locals()) and ((data['isFile'] == True) or (data['isFile'] == 'true' )):
+            myMedia.url = ''
+            old_chemin = myMedia.chemin
+            filepath = upload_file(file,  myMedia.id_media, myMedia.cd_ref, data['titre'])
+            myMedia.chemin = filepath
+
+            if (old_chemin != '') and (old_chemin != myMedia.chemin) :
+                remove_file(old_chemin)
+        elif ('url' in data) and (data['url'] != 'null') and (data['isFile'] != True) :
+            myMedia.url = data['url']
+            if myMedia.chemin != '' :
+                remove_file(myMedia.chemin)
+                myMedia.chemin = ''
+
+        db.session.add(myMedia)
+        db.session.commit()
+
+        return json.dumps({'success':True, 'id_media':myMedia.id_media, 'media' : myMedia.as_dict() }), 200, {'ContentType':'application/json'}
+
     except Exception as e:
-        print ('Exception')
-        db.session.rollback()
-        print (e)
-        return json.dumps({'success':False }), 500, {'ContentType':'application/json'}
-
-
-    id_media = myMedia.id_media
-
-    return json.dumps({'success':True, 'id_media':id_media, 'media' : myMedia.as_dict() }), 200, {'ContentType':'application/json'}
+        return json.dumps({'success':False, 'message':repr(e) }), 500, {'ContentType':'application/json'}
 
 @adresses.route('/<int:id_media>', methods=['DELETE'])
 @fnauth.check_auth(4)
@@ -150,9 +143,8 @@ def remove_file(filepath):
     except :
         pass
 
-def upload_file(file, cd_ref, titre):
-    print('upload_file')
-    filename = str(cd_ref)+ '_' + secure_filename(titre) + '.' + file.filename.rsplit('.', 1)[1]
+def upload_file(file, id_media, cd_ref, titre):
+    filename = str(cd_ref)+ '_' + str(id_media) + '_' + secure_filename(titre) + '.' + file.filename.rsplit('.', 1)[1]
     filepath = os.path.join(init_app().config['UPLOAD_FOLDER'], filename)
     file.save(os.path.join(init_app().config['BASE_DIR'], filepath))
     return filepath
