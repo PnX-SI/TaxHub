c007bf2e38c722d84fa31c649cf81e07b4de4cb6
amandine-sahl <amandine.sahl@gmail.com>
2016-09-05 11:36:36 +0200

    Ajout module : log action

---
+++ apptax/log/logmanager.py
@@ -0,0 +1,16 @@
+
+from .models import TaxhubAdminLog
+
+from server import db
+
+def log_action(id_role, object_type, object_id, object_repr,change_type,change_message):
+    log = TaxhubAdminLog (
+        id_role = id_role,
+        object_type=object_type,
+        object_id=object_id,
+        object_repr=object_repr,
+        change_type=change_type,
+        change_message=change_message
+    )
+    db.session.add(log)
+    db.session.commit()
---
+++ apptax/log/models.py
@@ -0,0 +1,16 @@
+#coding: utf8
+from server import db
+
+from ..utils.genericmodels import serializableModel
+
+class TaxhubAdminLog(serializableModel, db.Model):
+    __tablename__ = 'taxhub_admin_log'
+    __table_args__ = {'schema':'taxonomie'}
+    id = db.Column(db.Integer, primary_key=True)
+    action_time = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    id_role = db.Column(db.Integer, primary_key=True)
+    object_type = db.Column(db.Unicode)
+    object_id = db.Column(db.Integer, primary_key=True)
+    object_repr = db.Column(db.Unicode)
+    change_type = db.Column(db.Unicode)
+    change_message = db.Column(db.Unicode)
