08c05c431f931051351eaae5fbc33c6c0890da89
QuangPhamll <quang.pham.fr@gmail.com>
2017-04-19 13:00:15 +0200

    LISTE - Edit - restructurer le code de la partie angularjs

--- static/app/bib_liste/edit/bibliste-edit-controler.js
+++ static/app/bib_liste/edit/bibliste-edit-controler.js
@@ -41,46 +41,21 @@ app.controller('bibListeEditCtrl',[ '$scope','$filter', '$http','$uibModal','$ro
     $http.get(backendCfg.api_url+"biblistes/edit/picto_biblistes").then(function(response) {
         self.edit_picto_db = response.data;
     });
+//----------------------- Get list of nom_list
+    $http.get(backendCfg.api_url+"biblistes/edit/nom_liste").then(function(response) {
+        self.edit_nom_liste = response.data;
+    });
 //-----------------------Get list of picto in dossier ./static/images/pictos -----------------------------------------------
     $http.get(backendCfg.api_url+"biblistes/edit/picto_projet").then(function(response) {
         self.edit_picto_projet = response.data;
-//-------Get list of nom_list
-        $http.get(backendCfg.api_url+"biblistes/edit/nom_liste").then(function(response) {
-            self.edit_nom_liste = response.data;
-        });
-
-        // ----- compare the difference into 2 pictos listes: on database and in projet
-        // ----- then save the differeces pictos into an array.
-        // ----- use this array as the options for selection list on interface
         
-        for(i = 0; i < self.edit_picto_projet.length; i++)
-        {
-            var path = "images/pictos/"+self.edit_picto_projet[i];
-            for(j = 0; j < self.edit_picto_db.length; j++)
-            {
-                if(path.localeCompare(self.edit_picto_db[j]) == 0)
-                {
-                    break;
-                }
-                if(j == self.edit_picto_db.length - 1)
-                    self.pictos_propose.push(self.edit_picto_projet[i]) ;
-            }
-        }
-        // -- add nopicto
-        for(i = 0; i < self.pictos_propose.length; i++){
-            if(self.pictos_propose[i] === "nopicto.gif") 
-                break;
-            if(i === self.pictos_propose.length - 1) 
-                self.pictos_propose.push("nopicto.gif");
-        }
-        // -- add currently picto
-        var picto_string = self.edit_detailliste.picto.substring(14);
-        if(picto_string != "nopicto.gif")
-            self.pictos_propose.push(picto_string);
+        //--- call filter pcitos to get corresponds pictos on interface list of picto
+        self.pictos_propose = filterPictos(self.edit_picto_projet,self.edit_picto_db,self.edit_detailliste.picto);
 
         //----- stop spinner ------
         self.showSpinner = false;
     });
+
 
     var toasterMsg = {
         'saveSuccess':{"title":"Taxon enregistré", "msg": "Le taxon a été enregistré avec succès"},
@@ -88,8 +63,6 @@ app.controller('bibListeEditCtrl',[ '$scope','$filter', '$http','$uibModal','$ro
         'submitInfo_nothing_change':{"title":"L'Information de la liste ne change pas"},
         'saveError':{"title":"Erreur d'enregistrement"},
     }
-
-
 
     self.submit = function() {
         var flow = true;
@@ -126,8 +99,44 @@ app.controller('bibListeEditCtrl',[ '$scope','$filter', '$http','$uibModal','$ro
         }  
     }
 
+    //--- a paramettre of javascript array.filtre(para)
     function removeCurrentListName(value) {
         return value !=  list_prototype.nom_liste;
     }
     
+    //---- filtre pictos
+    function filterPictos(picto_projet,picto_db,edit_detailliste_picto){
+        var pictos_propose = [];
+
+        // ----- compare the difference into 2 pictos listes: on database and in projet
+        // ----- then save the differeces pictos into an array.
+        // ----- use this array as the options for selection list on interface
+        
+        for(i = 0; i < picto_projet.length; i++)
+        {
+            var path = "images/pictos/"+picto_projet[i];
+            for(j = 0; j < picto_db.length; j++)
+            {
+                if(path.localeCompare(picto_db[j]) == 0)
+                {
+                    break;
+                }
+                if(j == picto_db.length - 1)
+                    pictos_propose.push(picto_projet[i]) ;
+            }
+        }
+        // -- add nopicto
+        for(i = 0; i < pictos_propose.length; i++){
+            if(pictos_propose[i] === "nopicto.gif") 
+                break;
+            if(i === pictos_propose.length - 1) 
+                pictos_propose.push("nopicto.gif");
+        }
+        // -- add currently picto
+        var picto_string = edit_detailliste_picto.substring(14);
+        if(picto_string != "nopicto.gif")
+            pictos_propose.push(picto_string);
+        return pictos_propose;
+    }
+
 }]);
