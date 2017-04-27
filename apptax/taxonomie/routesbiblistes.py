f8bfb2b14e8965b327aad3b9f3b8bb694b8213ea
QuangPhamll <quang.pham.fr@gmail.com>
2017-04-27 17:50:24 +0200

    LISTES - Ajouter taxons - temporaire - tester performance  sql requete

--- apptax/taxonomie/routesbiblistes.py
+++ apptax/taxonomie/routesbiblistes.py
@@ -192,6 +192,30 @@ def get_exporter_liste(idliste = None):
 #TODO : idem discuter si mettre cette route dans routesbibnoms.py
 @adresses.route('/add/taxons', methods=['GET'])
 @json_resp
+# def get_bibtaxons():
+#     data = db.session.query(BibNoms).all()
+#     return [nom.as_dict() for nom in data]
+
+# def get_bibtaxons():
+#     data = db.session.query(BibNoms).all()
+#     taxonsList = []
+#     for r in data :
+#         obj = r.as_dict()
+#         #Ajout de taxref
+#         obj['taxref'] = r.taxref.as_dict()
+#         #obj['taxref'] = r.taxref.nom_complet
+
+#         taxonsList.append(obj)
+#     return taxonsList
+
 def get_bibtaxons():
-    data = db.session.query(BibNoms).all()
-    return [nom.as_dict() for nom in data]
+    data = db.engine.execute("\
+        select tbn.id_nom, tbn.cd_nom, tbn.nom_francais, tt.nom_complet \
+        from taxonomie.bib_noms tbn, taxonomie.taxref tt \
+        where tbn.cd_nom = tt.cd_nom").fetchall()
+    # results = []
+    # for row in data:
+    #     results.append(row[0])
+    #     results.append(row[1])
+
+    return data
