431616bfe36a66e458880c917931521618897f94
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-05-30 15:13:12 +0200

    SQL - Sortir les infos spécifiques des tables de Taxref

--- data/update1.1.2to1.2.0.sql
+++ data/update1.1.2to1.2.0.sql
@@ -8,3 +8,15 @@ DROP SEQUENCE taxonomie.bib_listes_id_liste_seq;
 --Rendre le picto de bib_listes obligatoire avec une valeur par défaut.
 ALTER TABLE taxonomie.bib_listes ALTER COLUMN picto SET NOT NULL;
 ALTER TABLE taxonomie.bib_listes ALTER COLUMN picto SET DEFAULT 'images/pictos/nopicto.gif';
+
+--Sortir les infos spécifiques des tables de Taxref
+CREATE TABLE taxonomie.taxref_protection_articles_structure
+(
+  cd_protection character varying(50) NOT NULL,
+  alias_statut character varying(10),
+  concerne_structure boolean,
+  CONSTRAINT taxref_protection_articles_structure_pkey PRIMARY KEY (cd_protection),
+  CONSTRAINT taxref_protection_articles_structure_cd_protect_fkey FOREIGN KEY (cd_protection)
+      REFERENCES taxonomie.taxref_protection_articles (cd_protection) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
