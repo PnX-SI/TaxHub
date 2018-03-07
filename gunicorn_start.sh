9c4b9a4ff68aeae93e18f88190805ab0f3a34f88
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-07 12:20:04 +0100

    correction lancement gunicorn

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -19,7 +19,5 @@ if [ "$enable_https" = true ]
 then
   exec gunicorn  server:app --certfile="${https_cert_path}" --keyfile="${https_key_path}" --access-logfile /var/log/taxhub/taxhub-access.log --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}" --bind 0.0.0:443
 else
-  exec gunicorn  server:app --access-logfile /var/log/taxhub/taxhub-access.log
-    --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}"
-    -b "${gun_host}:${gun_port}"  -n "${app_name}"
+  exec gunicorn  server:app --access-logfile /var/log/taxhub/taxhub-access.log --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}" -b "${gun_host}:${gun_port}"  -n "${app_name}"
 fi
