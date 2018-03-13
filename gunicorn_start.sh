4164349f075863d2cdf3fea907ee8cdc1d4a7f99
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-13 14:41:28 +0100

    remove https de gunicorn

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -15,9 +15,4 @@ export PYTHONPATH=$FLASKDIR:$PYTHONPATH
 
 
 # Start your unicorn
-if [ "$enable_https" = true ]
-then
-  exec gunicorn  server:app --certfile="${https_cert_path}" --keyfile="${https_key_path}" --access-logfile /var/log/taxhub/taxhub-access.log --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}" --bind 0.0.0:443
-else
-  exec gunicorn  server:app --access-logfile /var/log/taxhub/taxhub-access.log --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}" -b "${gun_host}:${gun_port}"  -n "${app_name}"
-fi
+exec gunicorn  server:app --access-logfile /var/log/taxhub/taxhub-access.log --error-log /var/log/taxhub/taxhub-errors.log --pid="${app_name}.pid" -w "${gun_num_workers}" -b "${gun_host}:${gun_port}"  -n "${app_name}"
