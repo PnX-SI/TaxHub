fd7c7c946b71ae250793def5abde68e74d2f1f7c
TheoLechemia <lechemia.theo@gmail.com>
2018-03-21 17:19:56 +0100

    SQL: passage bib_noms.nom_francais en char(1000)

--- data/taxhubdb.sql
+++ data/taxhubdb.sql
@@ -242,7 +242,7 @@ CREATE TABLE bib_noms (
     id_nom integer NOT NULL,
     cd_nom integer,
     cd_ref integer,
-    nom_francais character varying(255),
+    nom_francais character varying(1000),
     CONSTRAINT check_is_valid_cd_ref CHECK ((cd_ref = find_cdref(cd_ref)))
 );
 
