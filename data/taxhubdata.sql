1037c4ce29c9089cd63b2098d5dda6b326321da6
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-09-11 23:29:16 +0200

    DATA - Mise en forme, id et commentaires

--- data/taxhubdata.sql
+++ data/taxhubdata.sql
@@ -7,17 +7,20 @@ SET client_min_messages = warning;
 
 SET search_path = taxonomie, pg_catalog;
 
+-- Insertion d'un thème d'exemple permettant de gérer les attributs et informations taxonomiques spécifiques à un territoire
+INSERT INTO bib_themes (id_theme, nom_theme, desc_theme, ordre, id_droit) 
+     VALUES (1, 'Mon territoire', 'Informations relatives à mon territoire', 1, 4);
+SELECT setval('taxonomie.bib_themes_id_theme_seq', (SELECT max(id_theme)+1 FROM taxonomie.bib_themes), true);
 
-INSERT INTO bib_themes (id_theme, nom_theme, desc_theme, ordre, id_droit) VALUES (3, 'Mon territoire', 'Informations relatives à mon territoire', 3, 4);
-SELECT setval('taxonomie.bib_themes_id_theme_seq', (SELECT max(id_theme) FROM taxonomie.bib_themes), true);
+-- Insertion d'un attribut spécifique d'exemple (uniquement proposé pour les taxons du groupe Oiseaux)
+INSERT INTO bib_attributs (id_attribut, nom_attribut, label_attribut, liste_valeur_attribut, obligatoire, desc_attribut, type_attribut, type_widget, regne, group2_inpn, id_theme, ordre) 
+     VALUES (4, 'migrateur', 'Migrateur', '{"values":["migrateur","migrateur partiel","sédentaire"]}', false, 'Défini le statut de migration pour le territoire', 'varchar(50)', 'select', 'Animalia', 'Oiseaux', 1, 1);
 
+-- Insertion d'une liste permettant de définir les noms pouvant être saisis dans le module Occtax
+INSERT INTO bib_listes (id_liste, nom_liste,desc_liste,picto) 
+     VALUES (500,'Saisie Occtax','Liste des noms dont la saisie est proposée dans le module Occtax','images/pictos/nopicto.gif');
 
-INSERT INTO bib_attributs (id_attribut, nom_attribut, label_attribut, liste_valeur_attribut, obligatoire, desc_attribut, type_attribut, type_widget, regne, group2_inpn, id_theme, ordre) VALUES (4, 'migrateur', 'Migrateur', '{"values":["migrateur","migrateur partiel","sédentaire"]}', false, 'Défini le statut de migration pour le territoire', 'varchar(50)', 'select', 'Animalia', 'Oiseaux', 3, 200);
-
-
-INSERT INTO bib_listes (id_liste, nom_liste,desc_liste,picto) VALUES(500,'Saisie Occtax','Liste des noms dont la saisie est proposée dans le module Occtax','images/pictos/nopicto.gif');
-
-
+-- Insertion des types de média associables aux taxons
 INSERT INTO bib_types_media (id_type, nom_type_media, desc_type_media) VALUES (1, 'Photo_principale', 'Photo principale du taxon à utiliser pour les vignettes par exemple');
 INSERT INTO bib_types_media (id_type, nom_type_media, desc_type_media) VALUES (2, 'Photo', 'Autres photos du taxon');
 INSERT INTO bib_types_media (id_type, nom_type_media, desc_type_media) VALUES (3, 'Page web', 'URL d''une page web');
