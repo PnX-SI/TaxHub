4402537996303f37007eb443d593305645d28e19
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-09-11 23:24:07 +0200

    DATA ATLAS - Commentaires et séquence

--- data/taxhubdata_atlas.sql
+++ data/taxhubdata_atlas.sql
@@ -9,11 +9,12 @@ SET client_min_messages = warning;
 
 SET search_path = taxonomie, pg_catalog;
 
+-- Insertion du thème regroupant les attributs utilisés par GeoNature-atlas
+INSERT INTO bib_themes (id_theme, nom_theme, desc_theme, ordre, id_droit) 
+VALUES (2, 'Atlas', 'Informations relatives à GeoNature-atlas', 2, 3);
+SELECT setval('taxonomie.bib_themes_id_theme_seq', (SELECT max(id_theme)+1 FROM taxonomie.bib_themes), true);
 
-INSERT INTO bib_themes (id_theme, nom_theme, desc_theme, ordre, id_droit) VALUES (2, 'Atlas', 'Informations relatives à GeoNature-atlas', 2, 3);
-SELECT setval('taxonomie.bib_themes_id_theme_seq', (SELECT max(id_theme) FROM taxonomie.bib_themes), true);
-
-
+-- Insertion des attributs utilisés par GeoNature-atlas
 INSERT INTO bib_attributs (id_attribut, nom_attribut, label_attribut, liste_valeur_attribut, obligatoire, desc_attribut, type_attribut, type_widget, regne, group2_inpn, id_theme, ordre) VALUES (100, 'atlas_description', 'Description', '{}', false, 'Donne une description du taxon pour l''atlas', 'text', 'textarea', NULL, NULL, 2, 100);
 INSERT INTO bib_attributs (id_attribut, nom_attribut, label_attribut, liste_valeur_attribut, obligatoire, desc_attribut, type_attribut, type_widget, regne, group2_inpn, id_theme, ordre) VALUES (101, 'atlas_commentaire', 'Commentaire', '{}', false, 'Commentaire contextualisé sur le taxon pour GeoNature-Atlas', 'text', 'textarea', NULL, NULL, 2, 101);
 INSERT INTO bib_attributs (id_attribut, nom_attribut, label_attribut, liste_valeur_attribut, obligatoire, desc_attribut, type_attribut, type_widget, regne, group2_inpn, id_theme, ordre) VALUES (102, 'atlas_milieu', 'Milieu', '{"values":["Forêt","Prairie","eau"]}', false, 'Habitat, milieu principal du taxon', 'text', 'multiselect', NULL, NULL, 2, 102);
