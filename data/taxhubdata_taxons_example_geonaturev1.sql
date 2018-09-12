984928641c760da420c089542d6cc8f7fea88ec4
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-09-12 22:03:26 +0200

    Affinage données exemple et options
    Paramètres pour inserer les données exemple, l'attribut exemple...
    Renommage liste Saisie Occtax en id_liste = 100

--- data/taxhubdata_taxons_example_geonaturev1.sql
+++ data/taxhubdata_taxons_example_geonaturev1.sql
@@ -7,17 +7,7 @@ SET client_min_messages = warning;
 
 SET search_path = taxonomie, pg_catalog;
 
-
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (704, 67111, 67111, 'Ablette');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (64, 60612, 60612, 'Lynx boréal');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (23, 351, 351, 'Grenouille rousse');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (1950, 8326, 8326, 'Cicindela hybrida');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (2804, 11165, 11165, 'Coccinella septempunctata');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (816, 18437, 18437, 'Ecrevisse à pieds blancs');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (100001, 81065, 81065, 'Alchémille rampante');
-INSERT INTO bib_noms (id_nom, cd_nom, cd_ref, nom_francais) VALUES (100002, 95186, 95186, 'Inule fétide');
-
-
+-- Ajout des 8 taxons exemple aux listes nécéessaires à GeoNature V1
 INSERT INTO cor_nom_liste (id_nom ,id_liste) VALUES (704, 1001);
 INSERT INTO cor_nom_liste (id_nom ,id_liste) VALUES (64, 1001);
 INSERT INTO cor_nom_liste (id_nom ,id_liste) VALUES (23, 1001);
