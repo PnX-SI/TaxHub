f80702606450834158af9653766b6678bf7d2965
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-12-19 20:34:22 +0100

    adds_for_usershub.sql / Commentaires

--- data/adds_for_usershub.sql
+++ data/adds_for_usershub.sql
@@ -1,13 +1,13 @@
 
 SET search_path = utilisateurs, pg_catalog;
 
--- Insérer les applications de base liées à GeoNature
+-- Insérer les applications de base liées à TaxHub
 INSERT INTO t_applications (code_application, nom_application, desc_application, id_parent) VALUES 
-('TH', 'TaxHub', 'Application permettant d''administrer la liste des taxons.', NULL)
+('TH', 'TaxHub', 'Application permettant d''administrer les taxons.', NULL)
 ;
 SELECT pg_catalog.setval('t_applications_id_application_seq', (SELECT max(id_application)+1 FROM t_applications), false);	
 
---Définir les profils utilisables pour taxub
+--Définir les profils utilisables pour TaxHub
 INSERT INTO cor_profil_for_app (id_profil, id_application) VALUES
 (2, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'))
 ,(3, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')) 
