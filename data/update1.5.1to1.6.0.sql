71a5f9bdc943975edbc206defc820d085488f9cc
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2019-01-15 12:07:21 +0100

    Update update1.5.1to1.6.0.sql

--- data/update1.5.1to1.6.0.sql
+++ data/update1.5.1to1.6.0.sql
@@ -1,6 +1,3 @@
 -- Ajout d'un index sur la table vm_taxref_list_forautocomplete pour améliorer les performances
-
-SET search_path = taxonomie, pg_catalog;
-
 CREATE INDEX i_tri_vm_taxref_list_forautocomplete_search_name 
   ON taxonomie.vm_taxref_list_forautocomplete USING GIST (search_name gist_trgm_ops);
