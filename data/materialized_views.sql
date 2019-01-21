416e1e3e3cfe82943aed0a05c6c3b1e65977aa56
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-01-21 15:51:39 +0100

    index unique pour mettre le refresh des vues matérialisées

--- data/materialized_views.sql
+++ data/materialized_views.sql
@@ -104,3 +104,32 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS vm_ordre AS (SELECT DISTINCT ordre FROM t
 CREATE MATERIALIZED VIEW IF NOT EXISTS vm_famille AS (SELECT DISTINCT famille FROM taxref tx) WITH DATA;
 CREATE MATERIALIZED VIEW IF NOT EXISTS vm_group1_inpn AS (SELECT DISTINCT group1_inpn FROM taxref tx) WITH DATA;
 CREATE MATERIALIZED VIEW IF NOT EXISTS vm_group2_inpn AS (SELECT DISTINCT group2_inpn FROM taxref tx) WITH DATA;
+
+CREATE UNIQUE INDEX i_unique_ordre
+  ON taxonomie.vm_ordre
+  USING btree
+  (ordre);
+CREATE UNIQUE INDEX i_unique_phylum
+  ON taxonomie.vm_phylum
+  USING btree
+  (phylum);
+CREATE UNIQUE INDEX i_unique_regne
+  ON taxonomie.vm_regne
+  USING btree
+  (regne);
+CREATE UNIQUE INDEX i_unique_famille
+  ON taxonomie.vm_famille
+  USING btree
+  (famille);
+CREATE UNIQUE INDEX i_unique_classe
+  ON taxonomie.vm_classe
+  USING btree
+  (classe);
+CREATE UNIQUE INDEX i_unique_group1_inpn
+  ON taxonomie.vm_group1_inpn
+  USING btree
+  (group1_inpn);
+CREATE UNIQUE INDEX i_unique_group2_inpn
+  ON taxonomie.vm_group2_inpn
+  USING btree
+  (group2_inpn);
