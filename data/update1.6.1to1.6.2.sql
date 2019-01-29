8e2957a08f9793105fe36f2a344689a6323d7e29
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-01-29 16:28:09 +0100

    ajout rang + cd_nom dans autocomplete

--- data/update1.6.1to1.6.2.sql
+++ data/update1.6.1to1.6.2.sql
@@ -28,3 +28,108 @@ CREATE UNIQUE INDEX i_unique_group2_inpn
   ON taxonomie.vm_group2_inpn
   USING btree
   (group2_inpn);
+
+
+DROP TABLE taxonomie.vm_taxref_list_forautocomplete;
+
+CREATE TABLE taxonomie.vm_taxref_list_forautocomplete AS
+SELECT t.cd_nom,
+  t.cd_ref,
+  t.search_name,
+  t.nom_valide,
+  t.lb_nom,
+  t.regne,
+  t.group2_inpn,
+  l.id_liste
+FROM (
+  SELECT t_1.cd_nom,
+        t_1.cd_ref,
+        concat(t_1.lb_nom, ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_nom , ']') AS search_name,
+        t_1.nom_valide,
+        t_1.lb_nom,
+        t_1.regne,
+        t_1.group2_inpn
+  FROM taxonomie.taxref t_1
+  UNION
+  SELECT t_1.cd_nom,
+        t_1.cd_ref,
+        concat(n.nom_francais, ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_nom , ']' ) AS search_name,
+        t_1.nom_valide,
+        t_1.lb_nom,
+        t_1.regne,
+        t_1.group2_inpn
+  FROM taxonomie.taxref t_1
+  JOIN taxonomie.bib_noms n
+  ON t_1.cd_nom = n.cd_nom
+  WHERE n.nom_francais IS NOT NULL AND t_1.cd_nom = t_1.cd_ref
+) t
+JOIN taxonomie.v_taxref_all_listes l ON t.cd_nom = l.cd_nom;
+COMMENT ON TABLE vm_taxref_list_forautocomplete
+     IS 'Table construite à partir d''une requete sur la base et mise à jour via le trigger trg_refresh_mv_taxref_list_forautocomplete de la table cor_nom_liste';
+
+
+
+  -- Modification de la table vm_taxref_list_forautocomplete
+CREATE OR REPLACE FUNCTION taxonomie.trg_fct_refresh_mv_taxref_list_forautocomplete()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+	new_cd_nom int;
+	new_nom_vern varchar(1000);
+BEGIN
+	IF TG_OP in ('DELETE', 'TRUNCATE', 'UPDATE') THEN
+	    DELETE FROM taxonomie.vm_taxref_list_forautocomplete WHERE cd_nom IN (
+		SELECT cd_nom FROM taxonomie.bib_noms WHERE id_nom =  OLD.id_nom
+	    );
+	END IF;
+	IF TG_OP in ('INSERT', 'UPDATE') THEN
+		SELECT cd_nom, nom_francais INTO new_cd_nom, new_nom_vern FROM taxonomie.bib_noms WHERE id_nom = NEW.id_nom;
+
+		INSERT INTO taxonomie.vm_taxref_list_forautocomplete
+		SELECT t.cd_nom,
+            t.cd_ref,
+		    concat(t.lb_nom, ' =  <i> ', t.nom_valide, '</i>', '[', t.id_rang, ' - ', t.cd_nom , ']') AS search_name,
+		    t.nom_valide,
+		    t.lb_nom,
+		    t.regne,
+		    t.group2_inpn,
+		    NEW.id_liste
+		FROM taxonomie.taxref t  WHERE cd_nom = new_cd_nom;
+
+
+		IF NOT new_nom_vern IS NULL THEN
+			INSERT INTO taxonomie.vm_taxref_list_forautocomplete
+			SELECT t.cd_nom,
+                t.cd_ref,
+                concat(new_nom_vern, ' =  <i> ', t.nom_valide, '</i>', ' - [', t.id_rang, ' - ', t.cd_nom , ']') AS search_name,
+			    t.nom_valide,
+			    t.lb_nom,
+			    t.regne,
+			    t.group2_inpn,
+          NEW.id_liste
+			FROM taxonomie.taxref t
+			WHERE cd_nom = new_cd_nom AND t.cd_nom = t.cd_ref;
+		END IF;
+	END IF;
+  RETURN NEW;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
+  CREATE OR REPLACE FUNCTION taxonomie.trg_fct_refresh_nomfrancais_mv_taxref_list_forautocomplete()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+BEGIN
+    UPDATE taxonomie.vm_taxref_list_forautocomplete v
+    SET search_name = concat(NEW.nom_francais, ' =  <i> ', t.nom_valide, '</i>', ' - [', t.id_rang, ' - ', t.cd_nom , ']')
+    FROM taxonomie.taxref t
+		WHERE v.cd_nom = NEW.cd_nom AND t.cd_nom = NEW.cd_nom;
+    RETURN NEW;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
