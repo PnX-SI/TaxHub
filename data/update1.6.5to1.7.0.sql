82cd253b884f0327cd71f4671a9ab296853b571d
DonovanMaillard <donovan.maillard@gmail.com>
2020-05-19 08:44:38 +0200

    Update update1.6.5to1.7.0.sql

--- data/update1.6.5to1.7.0.sql
+++ data/update1.6.5to1.7.0.sql
@@ -16,8 +16,8 @@ AS $function$
 DECLARE 
 	matching_cd integer;
 BEGIN
-	IF (SELECT count(DISTINCT cd_nom) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name)=1 THEN matching_cd:= cd_nom FROM taxonomie.taxref WHERE lb_nom=my_taxa_name OR nom_valide=my_taxa_name ;
-	ELSIF (SELECT count(DISTINCT cd_ref) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name)=1 THEN matching_cd:= DISTINCT(cd_ref) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name OR nom_valide=my_taxa_name ;
+	IF (SELECT count(DISTINCT cd_nom) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name)=1 THEN matching_cd:= cd_nom FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name ;
+	ELSIF (SELECT count(DISTINCT cd_ref) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name)=1 THEN matching_cd:= DISTINCT(cd_ref) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name ;
 	ELSE matching_cd:= NULL;
 	END IF; 
 	RETURN matching_cd;
