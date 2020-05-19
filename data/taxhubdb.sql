218bbcca805851245f1db281081eb6c146cc20b1
DonovanMaillard <donovan.maillard@gmail.com>
2020-05-19 08:45:34 +0200

    Update taxhubdb.sql

--- data/taxhubdb.sql
+++ data/taxhubdb.sql
@@ -132,11 +132,11 @@ CREATE FUNCTION find_cdref(id integer) RETURNS integer
 $$;
 
 
-CREATE OR REPLACE FUNCTION match_binomial_taxref(my_taxa_name character varying)
+CREATE OR REPLACE FUNCTION taxonomie.match_binomial_taxref(my_taxa_name character varying)
 RETURNS integer
 LANGUAGE plpgsql
 IMMUTABLE
-AS $$
+AS $function$
 --fonction permettant de rattacher un nom latin aux cd_nom du taxref sur le principe suivant :
 -- - Si un seul cd_nom existe pour ce nom latin, la fonction retourne le cd_nom
 -- - Si plusieurs cd_noms existent pour ce nom latin, mais qu'ils appartiennent tous à un unique cd_ref, la fonction renvoie le cd_ref (= cd_nom valide)
@@ -144,13 +144,13 @@ AS $$
 DECLARE 
 	matching_cd integer;
 BEGIN
-	IF (SELECT count(DISTINCT cd_nom) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name)=1 THEN matching_cd:= cd_nom FROM taxonomie.taxref WHERE lb_nom=my_taxa_name OR nom_valide=my_taxa_name ;
-	ELSIF (SELECT count(DISTINCT cd_ref) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name)=1 THEN matching_cd:= DISTINCT(cd_ref) FROM taxonomie.taxref WHERE lb_nom=my_taxa_name OR nom_valide=my_taxa_name ;
+	IF (SELECT count(DISTINCT cd_nom) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name)=1 THEN matching_cd:= cd_nom FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name ;
+	ELSIF (SELECT count(DISTINCT cd_ref) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name)=1 THEN matching_cd:= DISTINCT(cd_ref) FROM taxonomie.taxref WHERE lb_nom ILIKE my_taxa_name OR nom_valide ILIKE my_taxa_name ;
 	ELSE matching_cd:= NULL;
 	END IF; 
 	RETURN matching_cd;
 END ;
-$$
+$function$
 ;
 
 
