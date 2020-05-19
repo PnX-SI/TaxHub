0b841ec35b2bbdb8712b7e66304a37d7e65bfc84
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2020-05-19 15:55:37 +0200

    TAXREF_V13 - correction copy avec superuser

--- data/scripts/update_taxref/import_taxref_v13_data.sh
+++ data/scripts/update_taxref/import_taxref_v13_data.sh
@@ -13,7 +13,7 @@ echo "Import des données de taxref v13"
 
 echo "Import des données de taxref v13" > $LOG_DIR/update_taxref_v13.log
 
-array=( TAXREF_INPN_v13.zip ESPECES_REGLEMENTEES_v13.zip BDC_STATUTS_13.zip)
+array=( TAXREF_INPN_v13.zip BDC_STATUTS_13.zip)
 for i in "${array[@]}"
 do
     if [ ! -f '/tmp/taxhub/'$i ]
@@ -27,18 +27,19 @@ done
 
 
 echo "Import taxref v13"
-export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.1_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
+export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.1.1_prepare_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/0.1.2_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
 
 echo "Export des cd_nom à modifier dans les données d'observations"
-export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.1.1_cd_nom_disparu_synthese_export.sql &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/0.1.3_cd_nom_disparu_synthese_export.sql &>> $LOG_DIR/update_taxref_v13.log
 
-echo "Traitement bib nom disparus"
+echo "Traitement bib_noms disparus"
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.2_correction_cd_nom_disparus.sql &>> $LOG_DIR/update_taxref_v13.log
 
 
 echo "Detection des changements"
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/1.1_taxref_changes_detections.sql &>> $LOG_DIR/update_taxref_v13.log
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/1.2_taxref_changes_detections_cas_actions.sql &>> $LOG_DIR/update_taxref_v13.log
-export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/1.3_taxref_changes_detections_export.sql &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/1.3_taxref_changes_detections_export.sql &>> $LOG_DIR/update_taxref_v13.log
 
 echo "Export des bilans réalisés dans tmp"
