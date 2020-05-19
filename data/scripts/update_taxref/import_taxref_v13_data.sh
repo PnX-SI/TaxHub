112fdee701d2555cbcbb58fcff3e583e1cbb1ae0
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2020-05-19 16:05:59 +0200

    TAXREF_V13 - mise à jour usage du user postgres

--- data/scripts/update_taxref/import_taxref_v13_data.sh
+++ data/scripts/update_taxref/import_taxref_v13_data.sh
@@ -28,10 +28,10 @@ done
 
 echo "Import taxref v13"
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.1.1_prepare_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
-sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/0.1.2_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -d $db_name  -f scripts/0.1.2_import_taxrefv13.sql   &>> $LOG_DIR/update_taxref_v13.log
 
 echo "Export des cd_nom à modifier dans les données d'observations"
-sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/0.1.3_cd_nom_disparu_synthese_export.sql &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -d $db_name  -f scripts/0.1.3_cd_nom_disparu_synthese_export.sql &>> $LOG_DIR/update_taxref_v13.log
 
 echo "Traitement bib_noms disparus"
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/0.2_correction_cd_nom_disparus.sql &>> $LOG_DIR/update_taxref_v13.log
@@ -40,6 +40,6 @@ export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scr
 echo "Detection des changements"
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/1.1_taxref_changes_detections.sql &>> $LOG_DIR/update_taxref_v13.log
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name  -f scripts/1.2_taxref_changes_detections_cas_actions.sql &>> $LOG_DIR/update_taxref_v13.log
-sudo -u postgres -s psql -h $db_host -U postgres -d $db_name  -f scripts/1.3_taxref_changes_detections_export.sql &>> $LOG_DIR/update_taxref_v13.log
+sudo -u postgres -s psql -h $db_host -d $db_name  -f scripts/1.3_taxref_changes_detections_export.sql &>> $LOG_DIR/update_taxref_v13.log
 
 echo "Export des bilans réalisés dans tmp"
