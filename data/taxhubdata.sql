2d6922354e50adb1cbecc92af995b6e2e8e0d0d0
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2021-06-18 16:01:28 +0200

    Revert "Revert "Take french name in vm_taxref_list_forautocomplete if nom_vern is null""
    This reverts commit 7748a216cbb1c6f055cf8aa5a83f92661c5afc8b.

--- data/taxhubdata.sql
+++ data/taxhubdata.sql
@@ -14,7 +14,7 @@ SELECT setval('taxonomie.bib_themes_id_theme_seq', (SELECT max(id_theme)+1 FROM 
 
 -- Insertion d'une liste permettant de définir les noms pouvant être saisis dans le module Occtax
 INSERT INTO bib_listes (id_liste, code_liste,  nom_liste,desc_liste,picto)
-    VALUES (100, '100', 'Saisie Occtax','Liste des noms dont la saisie est proposée dans le module Occtax','images/pictos/nopicto.gif');
+    VALUES (100,'OCCTAX', 'Saisie Occtax','Liste des noms dont la saisie est proposée dans le module Occtax','images/pictos/nopicto.gif');
 
 -- Insertion des types de média associables aux taxons
 INSERT INTO bib_types_media (id_type, nom_type_media, desc_type_media) VALUES (1, 'Photo_principale', 'Photo principale du taxon à utiliser pour les vignettes par exemple');
@@ -30,45 +30,44 @@ INSERT INTO bib_types_media (id_type, nom_type_media, desc_type_media) VALUES (9
 -- Creation d'une vue matérialis&ée de tous les noms de Taxref mis en forme pour la recherche de taxons
 DROP MATERIALIZED VIEW IF EXISTS taxonomie.vm_taxref_list_forautocomplete;
 
-CREATE MATERIALIZED VIEW taxonomie.vm_taxref_list_forautocomplete AS
-SELECT
-  row_number() OVER() as gid,
-  t.cd_nom,
-  t.cd_ref,
-  t.search_name,
-  t.nom_valide,
-  t.lb_nom,
-  t.nom_vern,
-  t.regne,
-  t.group2_inpn
-FROM (
-  -- PARTIE NOM SCIENTIFIQUE : ici on prend TOUS les synonymes.
-  SELECT
-    t_1.cd_nom,
-    t_1.cd_ref,
-    concat(t_1.lb_nom, ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_nom , ']') AS search_name,
-    t_1.nom_valide,
-    t_1.lb_nom,
-    t_1.nom_vern,
-    t_1.regne,
-    t_1.group2_inpn
-  FROM taxonomie.taxref t_1
-  UNION
-  -- PARTIE NOM FRANCAIS : ici on prend une seule fois (DISTINCT) dans Taxref tous les taxons de références
-  -- On ne prend pas les taxons qui n'ont pas de nom vern dans taxref,
-  -- donc si un taxon n'a pas de nom vern dans Taxref, il n'est accessible que par son nom scientifique.
-  SELECT DISTINCT
-    t_1.cd_nom,
-    t_1.cd_ref,
-    concat(split_part(t_1.nom_vern, ',', 1), ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_ref , ']' ) AS search_name,
-    t_1.nom_valide,
-    t_1.lb_nom,
-    t_1.nom_vern,
-    t_1.regne,
-    t_1.group2_inpn
-  FROM taxonomie.taxref t_1
-  WHERE t_1.nom_vern IS NOT null and t_1.cd_nom = t_1.cd_ref
-) t;
+CREATE materialized  VIEW taxonomie.vm_taxref_list_forautocomplete AS
+SELECT row_number() OVER () AS gid,
+    t.cd_nom,
+    t.cd_ref,
+    t.search_name,
+    t.nom_valide,
+    t.lb_nom,
+    t.nom_vern,
+    t.regne,
+    t.group2_inpn
+   FROM (
+     -- PARTIE NOM SCIENTIFIQUE : ici on prend TOUS les synonymes.
+
+   SELECT t_1.cd_nom,
+            t_1.cd_ref,
+            concat(t_1.lb_nom, ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_nom, ']') AS search_name,
+            t_1.nom_valide,
+            t_1.lb_nom,
+            t_1.nom_vern,
+            t_1.regne,
+            t_1.group2_inpn
+           FROM taxonomie.taxref t_1
+        union
+         -- PARTIE NOM FRANCAIS : ici on prend une seule fois (DISTINCT) dans Taxref tous les taxons de références
+        -- On ne prend pas les taxons qui n'ont pas de nom vern dans taxref,
+        -- donc si un taxon n'a pas de nom vern dans Taxref ou de nom_français dans bib_nom, il n'est accessible que par son nom scientifique.
+         SELECT DISTINCT t_1.cd_nom,
+            t_1.cd_ref,
+            concat(split_part(coalesce(bn.nom_francais, t_1.nom_vern::text), ','::text, 1), ' =  <i> ', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_ref, ']') AS search_name,
+            t_1.nom_valide,
+            t_1.lb_nom,
+            coalesce(bn.nom_francais, t_1.nom_vern),
+            t_1.regne,
+            t_1.group2_inpn
+           FROM taxonomie.taxref t_1
+           left join taxonomie.bib_noms bn on bn.cd_nom = t_1 .cd_nom
+          WHERE (t_1.nom_vern IS NOT null or bn.nom_francais is not null) AND t_1.cd_nom = t_1.cd_ref) t;
+
 COMMENT ON MATERIALIZED VIEW taxonomie.vm_taxref_list_forautocomplete
     IS 'Vue matérialisée permettant de faire des autocomplete construite à partir d''une requete sur tout taxref.';
 
@@ -76,10 +75,99 @@ COMMENT ON MATERIALIZED VIEW taxonomie.vm_taxref_list_forautocomplete
 CREATE unique index i_vm_taxref_list_forautocomplete_gid
   ON taxonomie.vm_taxref_list_forautocomplete (gid);
 CREATE INDEX i_vm_taxref_list_forautocomplete_cd_nom
-  ON vm_taxref_list_forautocomplete (cd_nom ASC NULLS LAST);
+  ON taxonomie.vm_taxref_list_forautocomplete (cd_nom ASC NULLS LAST);
 CREATE INDEX i_vm_taxref_list_forautocomplete_search_name
-  ON vm_taxref_list_forautocomplete (search_name ASC NULLS LAST);
+  ON taxonomie.vm_taxref_list_forautocomplete (search_name ASC NULLS LAST);
 CREATE INDEX i_tri_vm_taxref_list_forautocomplete_search_name
-  ON vm_taxref_list_forautocomplete
+  ON taxonomie.vm_taxref_list_forautocomplete
   USING gist
   (search_name  gist_trgm_ops);
+
+-- ##############################################"""""
+--------- BDC statuts
+-- ##############################################"""""
+
+
+--- ### populate
+-- bdc_statut_text
+ALTER  TABLE taxonomie.bdc_statut_text ADD id int[];
+
+INSERT INTO taxonomie.bdc_statut_text
+(cd_type_statut, cd_sig, cd_doc, niveau_admin, cd_iso3166_1, cd_iso3166_2, lb_adm_tr, full_citation, doc_url,  id)
+SELECT DISTINCT  cd_type_statut,
+	-- code_statut , label_statut ,
+	cd_sig , cd_doc , niveau_admin , cd_iso3166_1 , cd_iso3166_2 , lb_adm_tr,
+	full_citation, doc_url ,
+	array_agg(DISTINCT tbs.id) id
+FROM taxonomie.bdc_statut tbs
+GROUP BY  cd_type_statut,
+	-- code_statut , label_statut ,
+	cd_sig , cd_doc , niveau_admin , cd_iso3166_1 , cd_iso3166_2 , lb_adm_tr,
+	full_citation, doc_url ;
+
+UPDATE taxonomie.bdc_statut_text tbst  SET doc_url = NULL
+WHERE doc_url ='';
+
+-- bdc_statut_values
+ALTER  TABLE taxonomie.bdc_statut_values ADD id int[];
+ALTER  TABLE taxonomie.bdc_statut_values ADD ids_text int[];
+
+INSERT INTO taxonomie.bdc_statut_values (code_statut, label_statut, ids_text, id)
+SELECT DISTINCT tbs.code_statut , label_statut,  array_agg(DISTINCT t.id_text) ids_text,  array_agg(DISTINCT tbs.id) id
+FROM taxonomie.bdc_statut tbs
+JOIN taxonomie.bdc_statut_text t
+ON t.cd_type_statut = tbs.cd_type_statut
+	AND (t.cd_sig = tbs.cd_sig OR  t.cd_sig IS NULL)
+	AND t.full_citation = tbs.full_citation
+GROUP BY  tbs.code_statut , label_statut;
+
+-- bdc_statut_cor_text_values
+INSERT INTO taxonomie.bdc_statut_cor_text_values (id_value, id_text)
+SELECT id_value, unnest(ids_text) AS id_text
+FROM taxonomie.bdc_statut_values ;
+
+-- Mise en correspondances des textes, values et taxon
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_text;
+ALTER TABLE taxonomie.bdc_statut ADD id_text int;
+
+UPDATE taxonomie.bdc_statut s SET  id_text = a.id_text
+FROM (
+	SELECT unnest(id) AS id, id_text
+	FROM  taxonomie.bdc_statut_text
+)a
+WHERE a.id = s.id;
+
+
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_value ;
+ALTER TABLE taxonomie.bdc_statut ADD id_value int;
+UPDATE taxonomie.bdc_statut s SET  id_value = a.id_value
+FROM (
+	SELECT unnest(id) AS id, id_value
+	FROM  taxonomie.bdc_statut_values
+)a
+WHERE a.id = s.id;
+
+
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_value_text ;
+ALTER TABLE taxonomie.bdc_statut ADD id_value_text int;
+UPDATE taxonomie.bdc_statut s SET  id_value_text = c.id_value_text
+FROM taxonomie.bdc_statut_cor_text_values  c
+WHERE c.id_text = s.id_text AND s.id_value = c.id_value;
+
+-- bdc_statut_taxons
+INSERT INTO taxonomie.bdc_statut_taxons (id, id_value_text, cd_nom, cd_ref, rq_statut)
+SELECT id, id_value_text, t.cd_nom, t.cd_ref, rq_statut
+FROM  taxonomie.bdc_statut s
+JOIN taxonomie.taxref t
+ON s.cd_nom = t.cd_nom; -- Jointure sur taxref car 3 cd_nom n'existent pas : 847285, 973500, 851332
+
+
+--- ### populate
+ALTER  TABLE taxonomie.bdc_statut_text DROP id;
+
+ALTER  TABLE taxonomie.bdc_statut_values DROP id;
+ALTER  TABLE taxonomie.bdc_statut_values DROP ids_text;
+
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_value_text ;
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_value ;
+ALTER TABLE taxonomie.bdc_statut DROP IF EXISTS id_text;
