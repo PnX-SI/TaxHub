527c5e87354fb17d0dda16caa8e6b21a3103ed0f
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-06-21 09:35:55 +0200

    remove synthese dependencies

--- data/scripts/update_taxref/scripts/1.1_taxref_changes_detections.sql
+++ data/scripts/update_taxref/scripts/1.1_taxref_changes_detections.sql
@@ -1,5 +1,4 @@
-﻿
----- #################################################################################
+﻿---- #################################################################################
 ---- #################################################################################
 ----		Répercussion des changements taxonomiques 
 ---- #################################################################################
@@ -11,17 +10,9 @@ DROP TABLE IF EXISTS tmp_taxref_changes.comp_grap ;
 
 CREATE TABLE tmp_taxref_changes.comp_grap AS 
 WITH grappe_init AS (
-	SELECT b.cd_ref , sort(array_agg(b.cd_nom)) as array_agg, count(DISTINCT b.cd_nom)
+	SELECT b.cd_ref , sort(array_agg(cd_nom)) as array_agg, count(DISTINCT cd_nom)
 	FROM  taxonomie.bib_noms b
-	WHERE NOT deleted = true AND b.cd_nom in (
-		SELECT DISTINCT cd_nom from gn_synthese.synthese
-		UNION 
-		SELECT DISTINCT cd_ref 
-		FROM taxonomie.t_medias
-		UNION
-		SELECT DISTINCT cd_ref
-		FROM taxonomie.cor_taxon_attribut
-		)
+	WHERE NOT deleted = true
 	GROUP BY cd_ref
 ),
 grappe_final AS (
@@ -29,15 +20,7 @@ grappe_final AS (
 	FROM  taxonomie.bib_noms b
 	JOIN taxonomie.import_taxref t
 	ON b.cd_nom = t.cd_nom
-	WHERE NOT deleted = true AND b.cd_nom in (
-		SELECT DISTINCT cd_nom from gn_synthese.synthese
-		UNION 
-		SELECT DISTINCT cd_ref 
-		FROM taxonomie.t_medias
-		UNION
-		SELECT DISTINCT cd_ref
-		FROM taxonomie.cor_taxon_attribut
-		)
+	WHERE NOT deleted = true
 	GROUP BY t.cd_ref
 ),
 attribs AS (
@@ -63,4 +46,3 @@ LEFT OUTER JOIN  media m ON i.cd_ref = m.cd_ref;
 ALTER TABLE tmp_taxref_changes.comp_grap ADD grappe_change varchar(250);
 ALTER TABLE  tmp_taxref_changes.comp_grap ADD action varchar(250); 
 ALTER TABLE tmp_taxref_changes.comp_grap ADD cas varchar(50);
-
