4b7a18b4e31a6671e48cc97962c1bae0098086ce
JB Desbas <jb.desbas@gmail.com>
2021-06-23 10:43:02 +0200

    cd_nom -> cd_ref
    Renommer la colonne cd_nom en cd_ref pour plus de clarté

--- data/taxref_tree.sql
+++ data/taxref_tree.sql
@@ -1,25 +1,25 @@
---Construction
+--Construction de la VM
 
 CREATE EXTENSION IF NOT EXISTS ltree;
 
 CREATE MATERIALIZED VIEW taxonomie.taxref_tree
 AS WITH RECURSIVE x AS (
-         SELECT t.cd_nom,
+         SELECT t.cd_nom as cd_ref,
             t.cd_nom::text::ltree AS path
            FROM taxonomie.taxref t
           WHERE t.cd_sup IS NULL AND t.cd_nom = t.cd_ref
         UNION ALL
-         SELECT y.cd_nom,
+         SELECT y.cd_nom AS cd_ref,
             ltree_addtext(x_1.path, y.cd_nom::text) AS path
            FROM x x_1,
             taxonomie.taxref y
-          WHERE y.cd_nom = y.cd_ref AND x_1.cd_nom = y.cd_sup
+          WHERE y.cd_nom = y.cd_ref AND x_1.cd_ref = y.cd_sup
         )
- SELECT x.cd_nom,
+ SELECT x.cd_ref,
     x.path
    FROM x
 WITH DATA;
 
 -- View indexes:
-CREATE UNIQUE INDEX taxref_tree_cd_nom_idx ON taxonomie.taxref_tree USING btree (cd_nom);
+CREATE UNIQUE INDEX taxref_tree_cd_nom_idx ON taxonomie.taxref_tree USING btree (cd_ref);
 CREATE INDEX taxref_tree_path_idx ON taxonomie.taxref_tree USING gist (path); -- TRES important pour les perfs
