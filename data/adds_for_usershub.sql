4d7d980babac7f4105e376fd6fe7b48403e6191b
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-09-08 10:20:46 +0200

    remove hard-coded pk in sql

--- data/adds_for_usershub.sql
+++ data/adds_for_usershub.sql
@@ -1,20 +1,29 @@
-
-SET search_path = utilisateurs, pg_catalog;
-
 -- Insérer les applications de base liées à TaxHub
-INSERT INTO t_applications (code_application, nom_application, desc_application, id_parent) VALUES 
+INSERT INTO utilisateurs.t_applications (code_application, nom_application, desc_application, id_parent) VALUES 
 ('TH', 'TaxHub', 'Application permettant d''administrer les taxons.', NULL)
 ;
-SELECT pg_catalog.setval('t_applications_id_application_seq', (SELECT max(id_application)+1 FROM t_applications), false);
 
 --Définir les profils utilisables pour TaxHub
-INSERT INTO cor_profil_for_app (id_profil, id_application) VALUES
-(2, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'))
-,(3, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')) 
-,(4, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'))
-,(6, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'))
+INSERT INTO utilisateurs.cor_profil_for_app (id_profil, id_application) VALUES
+(
+    (SELECT id_profil FROM utilisateurs.t_profils WHERE code_profil = '2'),
+    (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')
+),(
+    (SELECT id_profil FROM utilisateurs.t_profils WHERE code_profil = '3'),
+    (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')
+),(
+    (SELECT id_profil FROM utilisateurs.t_profils WHERE code_profil = '4'),
+    (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')
+),(
+    (SELECT id_profil FROM utilisateurs.t_profils WHERE code_profil = '6'),
+    (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH')
+)
 ;
 
-INSERT INTO cor_role_app_profil (id_role, id_application, id_profil) VALUES
-(9, (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'), 6) --admin Taxhub
+INSERT INTO utilisateurs.cor_role_app_profil (id_role, id_application, id_profil) VALUES
+(
+    (SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Grp_admin'),
+    (SELECT id_application FROM utilisateurs.t_applications WHERE code_application = 'TH'),
+    (SELECT id_profil FROM utilisateurs.t_profils WHERE code_profil = '6')
+)
 ;
