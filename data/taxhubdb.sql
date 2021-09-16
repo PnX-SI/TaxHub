8e90cc62f54c4269a82cdcb193ac59a49983426f
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-09-16 16:16:41 +0200

    Fix data: add forgotten v_bdc_status SQL

--- data/taxhubdb.sql
+++ data/taxhubdb.sql
@@ -844,3 +844,19 @@ CREATE OR REPLACE VIEW v_taxref_all_listes AS
     d.id_liste
    FROM taxonomie.taxref t
      JOIN bib_nom_lst d ON t.cd_nom = d.cd_nom;
+
+CREATE OR REPLACE VIEW taxonomie.v_bdc_status AS
+  SELECT s.cd_nom, s.cd_ref, s.rq_statut, v.code_statut , v.label_statut,
+    t.cd_type_statut, ty.thematique, ty.lb_type_statut, ty.regroupement_type, 
+    cd_st_text, t.cd_sig, t.cd_doc, t.niveau_admin, t.cd_iso3166_1, t.cd_iso3166_2,
+    t.full_citation, t.doc_url, ty.type_value
+  FROM taxonomie.bdc_statut_taxons AS s
+    JOIN taxonomie.bdc_statut_cor_text_values AS c
+      ON s.id_value_text  = c.id_value_text
+    JOIN taxonomie.bdc_statut_text AS t
+      ON t.id_text  = c.id_text
+    JOIN taxonomie.bdc_statut_values AS v
+      ON v.id_value = c.id_value
+    JOIN taxonomie.bdc_statut_type AS ty
+      ON ty.cd_type_statut = t.cd_type_statut
+  WHERE t.ENABLE = true ;
