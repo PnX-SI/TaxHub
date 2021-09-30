71e9b0d535436ea1afc0758d3861fdb8e5cd6cf5
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2021-09-30 16:58:43 +0200

    Docker local setup

---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+#Création des répertoires systèmes
+
+cp config.py.docker config.py
+
+echo "préparation du fichier config.py..."
+sed -i "s/SQLALCHEMY_DATABASE_URI = .*$/SQLALCHEMY_DATABASE_URI = \"postgresql:\/\/$user_pg:$user_pg_pass@$db_host:$db_port\/$db_name\"/" config.py
+
+
+# rendre la commande nvm disponible
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#installation de node et npm et des librairies JS
+cd static/
+nvm install
+nvm use
+npm ci
+cd ..
+
+#Installation du virtual env
+echo "Installation du virtual env..."
+
+
+
+
+#création d'un fichier de configuration
+if [ ! -f static/app/constants.js ]; then
+  echo 'Fichier de configuration non existant'
+  cp static/app/constants.js.sample static/app/constants.js
+fi
+
+
+#affectation des droits sur le répertoire static/medias
+chmod -R 775 static/medias
+
+#Lancement de l'application
+export PYTHONPATH=$BASE_DIR:$PYTHONPATH
+export FLASK_APP=server
+exec gunicorn docker_server:app --pid="${app_name}.pid" -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}"
