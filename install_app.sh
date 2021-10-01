ba8b061593e3f227e75238c17e17a37fc1418e3e
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-10-01 09:43:32 +0200

    fix apache conf file: replace ${TAXHUB_DIR}

--- install_app.sh
+++ install_app.sh
@@ -59,9 +59,9 @@ envsubst '${USER} ${TAXHUB_DIR}' < taxhub.service | sudo tee /etc/systemd/system
 sudo systemctl daemon-reload || exit 1
 
 # Configuration apache
-sudo cp taxhub_apache.conf /etc/apache2/conf-available/taxhub.conf || exit 1
+envsubst '${TAXHUB_DIR}' < taxhub_apache.conf | sudo tee /etc/apache2/conf-available/taxhub.conf || exit 1
 sudo a2enmod proxy || exit 1
 sudo a2enmod proxy_http || exit 1
-# you may need a restart if proxy & proxy_http was not already enabled
+# you may need to restart apache2 if proxy & proxy_http was not already enabled
 
 echo "Vous pouvez maintenant démarrer TaxHub avec la commande : sudo systemctl start taxhub"
