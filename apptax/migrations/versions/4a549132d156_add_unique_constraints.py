242cd6ef7661880488c84e9ccf947948807c2e65
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-02-04 17:51:33 +0100

    Add constraints

---
+++ apptax/migrations/versions/4a549132d156_add_unique_constraints.py
@@ -0,0 +1,28 @@
+"""Add unique constraints
+
+Revision ID: 4a549132d156
+Revises: 4fb7e197d241
+Create Date: 2022-01-28 15:21:46.042686
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a549132d156'
+down_revision = '4fb7e197d241'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+        ALTER TABLE taxonomie.bib_attributs ADD CONSTRAINT unique_bib_attributs_nom_attribut UNIQUE (nom_attribut);
+        ALTER TABLE taxonomie.bib_themes  ADD CONSTRAINT unique_bib_themes_nom_theme UNIQUE (nom_theme);
+        ALTER TABLE taxonomie.cor_nom_liste ADD CONSTRAINT unique_cor_nom_liste_id_liste_id_nom UNIQUE (id_liste, id_nom);
+    """)
+
+
+def downgrade():
+    pass
