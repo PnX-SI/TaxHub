258ba011dfacaf1b6fa9b1b1f0278c7bcb3b0252
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-02-11 10:55:36 +0100

    Formatage et commentaires

--- apptax/migrations/taxref/commands.py
+++ apptax/migrations/taxref/commands.py
@@ -1,38 +1,35 @@
-
-from flask import (
-    Blueprint
-)
+from flask import Blueprint
 
 import importlib
 import click
 from zipfile import ZipFile
 from sqlalchemy import text
-from utils_flask_sqla.migrations.utils import  open_remote_file
+from utils_flask_sqla.migrations.utils import open_remote_file
 
 from apptax.database import db
-from .utils import (
-    test_missing_cd_nom,
-    save_data,
-    process_bib_noms,
-    detect_changes,
-    copy_from_csv
-)
+from .utils import test_missing_cd_nom, save_data, process_bib_noms, detect_changes, copy_from_csv
 from . import logger
-routes = Blueprint("taxref_migration", __name__, cli_group='taxref_migration')
+
+routes = Blueprint("taxref_migration", __name__, cli_group="taxref_migration")
 
 
+base_url = "http://geonature.fr/data/inpn/taxonomie/"
 
-base_url = 'http://geonature.fr/data/inpn/taxonomie/'
 
 @routes.cli.command()
 def update_taxref_v15():
     """
-        Procédure de migration de taxref
-            Taxref v14 vers v15
-            Test de la disparition des cd_noms
+    Procédure de migration de taxref
+        Taxref v14 vers v15
+        Test de la disparition des cd_noms
     """
     # Prerequis : deps_test_fk_dependencies_cd_nom
-    query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', '0.2_taxref_detection_repercussion_disparition_cd_nom.sql'))
+    query = text(
+        importlib.resources.read_text(
+            "apptax.migrations.data.migrate_taxref_version",
+            "0.2_taxref_detection_repercussion_disparition_cd_nom.sql",
+        )
+    )
     db.session.execute(query)
 
     # import taxref v15 data
@@ -44,6 +41,7 @@ def update_taxref_v15():
         # TODO ??? Force exit or not ???
         exit()
 
+    # Test missing cd_nom
     process_bib_noms()
 
     # Change detection and repport
@@ -51,14 +49,18 @@ def update_taxref_v15():
 
 
 @routes.cli.command()
-@click.option('--keep-oldtaxref', is_flag=True)
-@click.option('--keep-oldbdc', is_flag=True)
+@click.option("--keep-oldtaxref", is_flag=True)
+@click.option("--keep-oldbdc", is_flag=True)
 def apply_changes(keep_taxref, keep_bdc):
+    """Procédure de migration de taxref
+         Taxref v14 vers v15
+         Application des changements
+    :param keep_taxref: Indique si l'on souhaite concerver l'ancienne version du referentiel taxref
+    :type keep_taxref: boolean
+    :param keep_bdc:  Indique si l'on souhaite concerver l'ancienne version du referentiel bdc_status
+    :type keep_bdc: boolean
     """
-        Procédure de migration de taxref
-            Taxref v14 vers v15
-            Application des changements
-    """
+
     # test if deleted cd_nom can be correct without manual intervention
     if test_missing_cd_nom():
         logger.error("Some cd_nom will disappear without substitute. You can't continue")
@@ -79,45 +81,76 @@ def apply_changes(keep_taxref, keep_bdc):
     # Update taxref v15
     logger.info("Migration of taxref ...")
     try:
-        query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', '3.2_alter_taxref_data.sql'))
+        query = text(
+            importlib.resources.read_text(
+                "apptax.migrations.data.migrate_taxref_version", "3.2_alter_taxref_data.sql"
+            )
+        )
         db.session.execute(query)
         db.session.commit()
         logger.info("it's done")
     except Exception as e:
         logger.error(str(e))
 
-    import_dbc_status()
+    # Import bdc status data and insert into taxhub tables
+    import_and_format_dbc_status()
 
     # Clean DB
     logger.info("Clean DB")
-    query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', '5_clean_db.sql'))
+    query = text(
+        importlib.resources.read_text(
+            "apptax.migrations.data.migrate_taxref_version", "5_clean_db.sql"
+        )
+    )
     db.session.execute(query)
 
     db.session.commit()
 
+
 def import_taxref_v15():
+    """
+    Import des données brutes de taxref v15 en base
+    avant leur traitement
+    """
 
     logger.info("Import TAXREFv15 into tmp table…")
 
     # Préparation création de table temporaire permettant d'importer taxref
-    for sqlfile in ['0_taxrefv15_import_data.sql',]:
-        query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', sqlfile))
+    for sqlfile in [
+        "0_taxrefv15_import_data.sql",
+    ]:
+        query = text(
+            importlib.resources.read_text("apptax.migrations.data.migrate_taxref_version", sqlfile)
+        )
         db.session.execute(query)
     db.session.commit()
 
-
-    with open_remote_file(base_url, 'TAXREF_v15_2021.zip', open_fct=ZipFile, data_dir="/home/sahl/dev/TaxHub/tmp") as archive:
-        with archive.open('TAXREFv15.txt') as f:
+    with open_remote_file(
+        base_url, "TAXREF_v15_2021.zip", open_fct=ZipFile, data_dir="/home/sahl/dev/TaxHub/tmp"
+    ) as archive:
+        with archive.open("TAXREFv15.txt") as f:
             logger.info("Insert TAXREFv15 tmp table…")
-            copy_from_csv(f, db.engine,
-                table_name="import_taxref", schema_name="taxonomie",
-                header=True, encoding=None, delimiter="\t", dest_cols=''
+            copy_from_csv(
+                f,
+                db.engine,
+                table_name="cdnom_disparu",
+                schema_name="taxonomie",
+                header=True,
+                encoding=None,
+                delimiter="\t",
+                dest_cols="",
             )
-        with archive.open('CDNOM_DISPARUS.csv') as f:
+        with archive.open("CDNOM_DISPARUS.csv") as f:
             logger.info("Insert cdnom_disparu tmp table…")
-            copy_from_csv(f, db.engine,
-                table_name="cdnom_disparu", schema_name="taxonomie",
-                header=True, encoding=None, delimiter=",", dest_cols=''
+            copy_from_csv(
+                f,
+                db.engine,
+                table_name="cdnom_disparu",
+                schema_name="taxonomie",
+                header=True,
+                encoding=None,
+                delimiter=",",
+                dest_cols="",
             )
 
         # No changes in taxref v15
@@ -128,12 +161,19 @@ def import_taxref_v15():
         #         header=True, encoding="WIN1252", delimiter=";", dest_cols=''
         #     )
 
-def import_dbc_status():
+
+def import_and_format_dbc_status():
+    """
+    Import des données brutes de la base bdc_status  en base
+    Puis traitement des données de façon à les ventiler dans les différentes tables
+    """
 
     import_data_dbc_status_15()
     # Delete doublons
     logger.info("Remove duplicate bdc_statut")
-    db.session.execute(text("""
+    db.session.execute(
+        text(
+            """
         --- Suppression des données en double contenu dans la table  bdc_statut
         CREATE INDEX bdc_statut_id_idx ON taxonomie.bdc_statut (id);
 
@@ -157,31 +197,56 @@ def import_dbc_status():
 
         DROP INDEX taxonomie.bdc_statut_id_idx;
 
-    """))
+    """
+        )
+    )
 
     # Structure BDC_Statut
     logger.info("Import raw bdc_statut into structured table")
-    query = text(importlib.resources.read_text('apptax.migrations.data', 'taxonomie_bdc_statuts.sql'))
+    query = text(
+        importlib.resources.read_text("apptax.migrations.data", "taxonomie_bdc_statuts.sql")
+    )
     db.session.execute(query)
 
+
 def import_data_dbc_status_15():
-    db.session.execute(text("""
+    """
+    Import des données brutes de la base bdc_status v14  en base
+    """
+    db.session.execute(
+        text(
+            """
         TRUNCATE TABLE  taxonomie.bdc_statut_type CASCADE;
         TRUNCATE TABLE  taxonomie.bdc_statut;
-    """))
+    """
+        )
+    )
     db.session.commit()
 
-    with open_remote_file(base_url, 'BDC-statuts-15.zip', open_fct=ZipFile, data_dir="/home/sahl/dev/TaxHub/tmp") as archive:
-        with archive.open('BDC_STATUTS_TYPES_15.csv') as f:
+    with open_remote_file(
+        base_url, "BDC-statuts-15.zip", open_fct=ZipFile, data_dir="/home/sahl/dev/TaxHub/tmp"
+    ) as archive:
+        with archive.open("BDC_STATUTS_TYPES_15.csv") as f:
             logger.info("Insert BDC_STATUTS_TYPES_15 table…")
-            copy_from_csv(f, db.engine,
-                table_name="bdc_statut_type", schema_name="taxonomie",
-                header=True, encoding=None, delimiter=",", dest_cols=''
+            copy_from_csv(
+                f,
+                db.engine,
+                table_name="bdc_statut_type",
+                schema_name="taxonomie",
+                header=True,
+                encoding=None,
+                delimiter=",",
+                dest_cols="",
             )
-        with archive.open('BDC_STATUTS_15.csv') as f:
+        with archive.open("BDC_STATUTS_15.csv") as f:
             logger.info("Insert bdc_statut table…")
-            copy_from_csv(f, db.engine,
-                table_name="bdc_statut", schema_name="taxonomie",
-                header=True, encoding="WIN1252", delimiter=",",
-                dest_cols='(cd_nom, cd_ref, cd_sup, cd_type_statut, lb_type_statut, regroupement_type, code_statut, label_statut, rq_statut, cd_sig, cd_doc, lb_nom, lb_auteur, nom_complet_html, nom_valide_html, regne, phylum, classe, ordre, famille, group1_inpn, group2_inpn, lb_adm_tr, niveau_admin, cd_iso3166_1, cd_iso3166_2, full_citation, doc_url, thematique, type_value)'
+            copy_from_csv(
+                f,
+                db.engine,
+                table_name="bdc_statut",
+                schema_name="taxonomie",
+                header=True,
+                encoding="WIN1252",
+                delimiter=",",
+                dest_cols="(cd_nom, cd_ref, cd_sup, cd_type_statut, lb_type_statut, regroupement_type, code_statut, label_statut, rq_statut, cd_sig, cd_doc, lb_nom, lb_auteur, nom_complet_html, nom_valide_html, regne, phylum, classe, ordre, famille, group1_inpn, group2_inpn, lb_adm_tr, niveau_admin, cd_iso3166_1, cd_iso3166_2, full_citation, doc_url, thematique, type_value)",
             )
--- apptax/migrations/taxref/utils.py
+++ apptax/migrations/taxref/utils.py
@@ -1,4 +1,3 @@
-
 import csv
 import importlib
 from zipfile import ZipFile
@@ -12,32 +11,47 @@ from .queries import (
     EXPORT_QUERIES_MISSING_CD_NOMS_IN_DB,
     EXPORT_QUERIES_MODIFICATION_NB,
     EXPORT_QUERIES_MODIFICATION_LIST,
-    EXPORT_QUERIES_CONFLICTS
+    EXPORT_QUERIES_CONFLICTS,
 )
 
 
 def process_bib_noms():
     """
-        Création d'une table copie de bib_noms
+    Création d'une table copie de bib_noms
     """
     logger.info("Create working copy of bib_noms…")
 
     # Préparation création de table temporaire permettant d'importer taxref
-    for sqlfile in ['0.1_generate_tmp_bib_noms_copy.sql',]:
-        query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', sqlfile))
+    for sqlfile in [
+        "0.1_generate_tmp_bib_noms_copy.sql",
+    ]:
+        query = text(
+            importlib.resources.read_text("apptax.migrations.data.migrate_taxref_version", sqlfile)
+        )
         db.session.execute(query)
     db.session.commit()
 
+
 def detect_changes():
+    """Detection des changements et de leur implication
+        sur bib_noms, les attributs et les médias
+
+    :return: Nombre de conflit detecté
+    :rtype: int
+    """
     # Analyse des changements
     for sqlfile in [
-        '1.1_taxref_changes_detections.sql',
-        '2.1_taxref_changes_corrections_pre_detections.sql',
-        '1.2_taxref_changes_detections_cas_actions.sql',
-        '2.2_taxref_changes_corrections_post_detections.sql'
+        "1.1_taxref_changes_detections.sql",
+        "2.1_taxref_changes_corrections_pre_detections.sql",
+        "1.2_taxref_changes_detections_cas_actions.sql",
+        "2.2_taxref_changes_corrections_post_detections.sql",
     ]:
         try:
-            query = text(importlib.resources.read_text('apptax.migrations.data.migrate_taxref_version', sqlfile))
+            query = text(
+                importlib.resources.read_text(
+                    "apptax.migrations.data.migrate_taxref_version", sqlfile
+                )
+            )
             db.session.execute(query)
         except FileNotFoundError:
             logger.warning(f"File not found {sqlfile}")
@@ -45,16 +59,10 @@ def detect_changes():
 
     # Export des changements
     results = db.session.execute(text(EXPORT_QUERIES_MODIFICATION_NB))
-    export_as_csv(
-        file_name="nb_changements.csv",
-        columns=results.keys(),
-        data=results.fetchall()
-    )
+    export_as_csv(file_name="nb_changements.csv", columns=results.keys(), data=results.fetchall())
     results = db.session.execute(text(EXPORT_QUERIES_MODIFICATION_LIST))
     export_as_csv(
-        file_name="liste_changements.csv",
-        columns=results.keys(),
-        data=results.fetchall()
+        file_name="liste_changements.csv", columns=results.keys(), data=results.fetchall()
     )
 
     logger.info(f"List of taxref changes done in tmp")
@@ -63,83 +71,120 @@ def detect_changes():
     nb_of_conflict = results.fetchone()["count"]
     return nb_of_conflict
 
-def save_data(version, keep_taxref, keep_bdc):
 
+def save_data(version, keep_taxref, keep_bdc):
+    """Sauvegarde des données de l'ancienne version de taxref
+
+    :param version: numéro de version de taxref
+    :type version: int
+    :param keep_taxref: Indique si l'on souhaite concerver l'ancienne version du referentiel taxref
+    :type keep_taxref: boolean
+    :param keep_bdc:  Indique si l'on souhaite concerver l'ancienne version du referentiel bdc_status
+    :type keep_bdc: boolean
+    """
     if keep_taxref:
-        db.session.execute(text(f"""
+        db.session.execute(
+            text(
+                f"""
             DROP TABLE IF EXISTS taxonomie.taxref_v{str(version)};
             CREATE TABLE taxonomie.taxref_v{str(version)} AS
             SELECT * FROM taxonomie.taxref;
-        """))
+        """
+            )
+        )
 
     if keep_bdc:
-        db.session.execute(text(f"""
+        db.session.execute(
+            text(
+                f"""
             DROP TABLE IF EXISTS taxonomie.bdc_statut_v{str(version)};
             CREATE TABLE taxonomie.bdc_statut_v{str(version)} AS
             SELECT * FROM taxonomie.bdc_statut;
-        """))
-        db.session.execute(text(f"""
+        """
+            )
+        )
+        db.session.execute(
+            text(
+                f"""
             DROP TABLE IF EXISTS taxonomie.bdc_statut_type_v{str(version)};
             CREATE TABLE taxonomie.bdc_statut_type_v{str(version)} AS
             SELECT * FROM taxonomie.bdc_statut_type;
-        """))
+        """
+            )
+        )
+
 
 def missing_cd_nom_query(query_name, export_file_name):
     results = db.session.execute(text(query_name))
     data = results.fetchall()
-    if len(data)>0:
-        logger.warning(f"Some cd_nom referencing in gn_synthese.synthese where missing from taxref v15 -> see file {export_file_name}")
+    if len(data) > 0:
+        logger.warning(
+            f"Some cd_nom referencing in gn_synthese.synthese where missing from taxref v15 -> see file {export_file_name}"
+        )
         export_as_csv(file_name=export_file_name, columns=results.keys(), data=data)
     # Test cd_nom without cd_nom_remplacement
     return test_cd_nom_without_sustitute(data)
 
 
 def test_cd_nom_without_sustitute(data, key="cd_nom_remplacement"):
-        for d in data:
-            # Si la requete ne comporte pas le champ cd_nom_remplacement
-            if not "cd_nom_remplacement" in d:
-                return False
-            if not d["cd_nom_remplacement"]:
-                return True
-        return False
+    for d in data:
+        # Si la requete ne comporte pas le champ cd_nom_remplacement
+        if not "cd_nom_remplacement" in d:
+            return False
+        if not d["cd_nom_remplacement"]:
+            return True
+    return False
+
 
 def test_missing_cd_nom():
     # test cd_nom disparus
     missing_cd_nom_bib_noms = missing_cd_nom_query(
-        query_name = EXPORT_QUERIES_MISSING_CD_NOMS_IN_BIB_NOMS,
-         export_file_name="missing_cd_nom_into_bib_nom.csv"
+        query_name=EXPORT_QUERIES_MISSING_CD_NOMS_IN_BIB_NOMS,
+        export_file_name="missing_cd_nom_into_bib_nom.csv",
     )
 
     # TODO => Delete redondonance avec EXPORT_QUERIES_MISSING_CD_NOMS_IN_DB
     missing_cd_nom_gn2 = missing_cd_nom_query(
-        query_name = EXPORT_QUERIES_MISSING_CD_NOM_GN2_SYNTHESE,
-        export_file_name="missing_cd_nom_into_gn_synthese.csv"
+        query_name=EXPORT_QUERIES_MISSING_CD_NOM_GN2_SYNTHESE,
+        export_file_name="missing_cd_nom_into_gn_synthese.csv",
     )
     missing_cd_nom_gn2 = missing_cd_nom_query(
-        query_name = EXPORT_QUERIES_MISSING_CD_NOMS_IN_DB,
-        export_file_name="missing_cd_nom_into_geonature.csv"
+        query_name=EXPORT_QUERIES_MISSING_CD_NOMS_IN_DB,
+        export_file_name="missing_cd_nom_into_geonature.csv",
     )
     return missing_cd_nom_bib_noms + missing_cd_nom_gn2
 
 
-def copy_from_csv(f, engine,
-    table_name, schema_name="taxonomie",
-    header=True, encoding=None, delimiter=None, dest_cols=''
+def copy_from_csv(
+    f,
+    engine,
+    table_name,
+    schema_name="taxonomie",
+    header=True,
+    encoding=None,
+    delimiter=None,
+    dest_cols="",
 ):
     try:
 
         options = ["FORMAT CSV"]
-        if header: options.append("HEADER")
-        if encoding: options.append(f"ENCODING '{encoding}'")
-        if delimiter: options.append(f"DELIMITER E'{delimiter}'")
-        options = ', '.join(options)
+        if header:
+            options.append("HEADER")
+        if encoding:
+            options.append(f"ENCODING '{encoding}'")
+        if delimiter:
+            options.append(f"DELIMITER E'{delimiter}'")
+        options = ", ".join(options)
 
         conn = engine.raw_connection()
         cursor = conn.cursor()
-        cursor.copy_expert(f"""
+        cursor.copy_expert(
+            f"""
             COPY {schema_name}.{table_name}{dest_cols}
             FROM STDIN WITH ({options})
-        """, f)
+        """,
+            f,
+        )
 
         conn.commit()
         cursor.close()
@@ -149,7 +194,8 @@ def copy_from_csv(f, engine,
     finally:
         conn.close()
 
-def export_as_csv(file_name, columns, data, separator=','):
+
+def export_as_csv(file_name, columns, data, separator=","):
     with open(f"tmp/{file_name}", "w") as f:
         writer = csv.writer(f)
         writer.writerow(columns)
