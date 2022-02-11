c63ab2cd64025a6ab32322c90b2c753a7f52b0da
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-02-11 10:54:22 +0100

    Suppression des scripts de customisation des traitements pre et post detection

--- apptax/migrations/data/migrate_taxref_version/2.1_taxref_changes_corrections_pre_detections.sql
+++
@@ -1,3 +0,0 @@
---- Exemple pour le pnc
-
-DELETE FROM taxonomie.cor_taxon_attribut WHERE cd_ref = 234640 and id_attribut=110;
--- apptax/migrations/data/migrate_taxref_version/2.2_taxref_changes_corrections_post_detections.sql
+++
@@ -1,4 +0,0 @@
---- Exemple pour le pnc
-
-UPDATE gn_synthese.synthese s SET cd_nom = 123485 WHERE cd_nom = 123486;
-UPDATE gn_synthese.synthese s SET cd_nom = 611926 WHERE cd_nom = 130781;
