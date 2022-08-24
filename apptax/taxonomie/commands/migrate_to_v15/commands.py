8ce29e691fdb21330eb89b7ef4cca407ebe5bc39
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-08-24 13:03:33 +0200

    rename command migrate_v14_to_v15 → migrate_to_v15

--- apptax/taxonomie/commands/migrate_to_v15/commands.py
+++ apptax/taxonomie/commands/migrate_to_v15/commands.py
@@ -18,11 +18,11 @@ base_url = "http://geonature.fr/data/inpn/taxonomie/"
 
 
 @click.group(help="Migrate from TaxRef v14 to TaxRef v15.")
-def migrate_v14_to_v15():
+def migrate_to_v15():
     pass
 
 
-@migrate_v14_to_v15.command()
+@migrate_to_v15.command()
 @with_appcontext
 def import_taxref_v15():
     """
@@ -47,7 +47,7 @@ def import_taxref_v15():
     analyse_taxref_changes()
 
 
-@migrate_v14_to_v15.command()
+@migrate_to_v15.command()
 @click.option("--keep-cdnom", is_flag=True)
 @with_appcontext
 def test_changes_detection(keep_cdnom):
@@ -66,7 +66,7 @@ def test_changes_detection(keep_cdnom):
     analyse_taxref_changes(without_substitution=False, keep_missing_cd_nom=keep_cdnom)
 
 
-@migrate_v14_to_v15.command()
+@migrate_to_v15.command()
 @click.option("--keep-oldtaxref", is_flag=True)
 @click.option("--keep-oldbdc", is_flag=True)
 @click.option("--keep-cdnom", is_flag=True)
--- apptax/taxonomie/commands/taxref.py
+++ apptax/taxonomie/commands/taxref.py
@@ -6,7 +6,7 @@ from apptax.taxonomie.models import Taxref, TaxrefBdcStatutText
 
 from .taxref_v14 import import_v14
 from .taxref_v15 import import_v15
-from .v14_to_v15.commands import migrate_v14_to_v15
+from .migrate_to_v15.commands import migrate_to_v15
 
 
 @click.group(help="Manager TaxRef referentials.")
@@ -63,4 +63,4 @@ def delete():
 
 taxref.add_command(import_v14)
 taxref.add_command(import_v15)
-taxref.add_command(migrate_v14_to_v15)
+taxref.add_command(migrate_to_v15)
