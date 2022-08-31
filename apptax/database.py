ad7988915cf5e1a08b24719327ea75fdde67ca44
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-08-31 15:07:00 +0200

    fix flask reload issue (2)

--- apptax/database.py
+++ apptax/database.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db_path = environ.get("FLASK_SQLALCHEMY_DB")
-if db_path and not environ.get("WERKZEUG_RUN_MAIN") == "true":
+if db_path and db_path != f"{__name__}.db":
     db_module_name, db_object_name = db_path.rsplit(".", 1)
     db_module = import_module(db_module_name)
     db = getattr(db_module, db_object_name)
