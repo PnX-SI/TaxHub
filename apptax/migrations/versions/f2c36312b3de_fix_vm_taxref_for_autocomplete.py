58825b66e3eb80b0458cfe2972fdea2226d579f0
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-10-18 10:12:23 +0200

    fix view

--- apptax/migrations/versions/f2c36312b3de_fix_vm_taxref_for_autocomplete.py
+++ apptax/migrations/versions/f2c36312b3de_fix_vm_taxref_for_autocomplete.py
@@ -24,14 +24,14 @@ def upgrade():
             TABLESPACE pg_default
             AS 
             SELECT row_number() OVER () AS gid,
-                t.cd_nom,
-                t.cd_ref,
-                t.search_name,
-                t.nom_valide,
-                t.lb_nom,
-                t.nom_vern,
-                t.regne,
-                t.group2_inpn
+            t.cd_nom,
+            t.cd_ref,
+            t.search_name,
+            t.nom_valide,
+            t.lb_nom,
+            t.nom_vern,
+            t.regne,
+            t.group2_inpn
             FROM ( SELECT t_1.cd_nom,
                         t_1.cd_ref,
                         concat(t_1.lb_nom, ' = <i>', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_nom, ']') AS search_name,
@@ -44,14 +44,15 @@ def upgrade():
                     UNION
                     SELECT DISTINCT t_1.cd_nom,
                         t_1.cd_ref,
-                        concat(split_part(t_1.nom_vern::text, ','::text, 1), COALESCE(t_1.nom_vern, '=', ''), '<i>', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_ref, ']') AS search_name,
+                        concat(split_part(t_1.nom_vern::text, ','::text, 1), ' =<i>', t_1.nom_valide, '</i>', ' - [', t_1.id_rang, ' - ', t_1.cd_ref, ']') AS search_name,
                         t_1.nom_valide,
                         t_1.lb_nom,
                         t_1.nom_vern,
                         t_1.regne,
                         t_1.group2_inpn
                     FROM taxonomie.taxref t_1
-                    WHERE t_1.nom_vern IS NOT NULL AND t_1.cd_nom = t_1.cd_ref) t          
+                    WHERE t_1.nom_vern IS NOT NULL AND t_1.cd_nom = t_1.cd_ref
+                    )t 
             WITH DATA;
         """
     )
