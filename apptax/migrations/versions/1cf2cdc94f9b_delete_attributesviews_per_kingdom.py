e5f2bf1511c40264346a95fb6e84b5b1f3b972c5
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-11-22 18:46:43 +0100

    Feat taxhub v2 : Interface graphique flask-admin

---
+++ apptax/migrations/versions/1cf2cdc94f9b_delete_attributesviews_per_kingdom.py
@@ -0,0 +1,92 @@
+"""Delete attributesviews_per_kingdom
+
+Revision ID: 1cf2cdc94f9b
+Revises: f6abb7857493
+Create Date: 2023-03-23 09:50:20.148765
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "1cf2cdc94f9b"
+down_revision = "f6abb7857493"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        DROP FUNCTION taxonomie.fct_build_bibtaxon_attributs_view(sregne character varying);
+        DROP FUNCTION taxonomie.trg_fct_refresh_attributesviews_per_kingdom() CASCADE;
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION taxonomie.fct_build_bibtaxon_attributs_view(sregne character varying)
+        RETURNS void
+        LANGUAGE plpgsql
+        AS $function$
+        DECLARE
+            r taxonomie.bib_attributs%rowtype;
+            sql_select text;
+            sql_join text;
+            sql_where text;
+        BEGIN
+            sql_join :=' FROM taxonomie.bib_noms b JOIN taxonomie.taxref taxref USING(cd_nom) ';
+            sql_select := 'SELECT b.* ';
+            sql_where := ' WHERE regne=''' ||$1 || '''';
+            FOR r IN
+                SELECT id_attribut, nom_attribut, label_attribut, liste_valeur_attribut,
+                    obligatoire, desc_attribut, type_attribut, type_widget, regne,
+                    group2_inpn
+                FROM taxonomie.bib_attributs
+                WHERE regne IS NULL OR regne=sregne
+            LOOP
+                sql_select := sql_select || ', ' || r.nom_attribut || '.valeur_attribut::' || r.type_attribut || ' as ' || r.nom_attribut;
+                sql_join := sql_join || ' LEFT OUTER JOIN (SELECT valeur_attribut, cd_ref FROM taxonomie.cor_taxon_attribut WHERE id_attribut= '
+                    || r.id_attribut || ') as  ' || r.nom_attribut || '  ON b.cd_ref= ' || r.nom_attribut || '.cd_ref ';
+
+            --RETURN NEXT r; -- return current row of SELECT
+            END LOOP;
+            EXECUTE 'DROP VIEW IF EXISTS taxonomie.v_bibtaxon_attributs_' || sregne ;
+            EXECUTE 'CREATE OR REPLACE VIEW taxonomie.v_bibtaxon_attributs_' || sregne ||  ' AS ' || sql_select || sql_join || sql_where ;
+        END
+        $function$
+        ;
+
+        CREATE OR REPLACE FUNCTION taxonomie.trg_fct_refresh_attributesviews_per_kingdom()
+        RETURNS trigger AS
+        $$
+        DECLARE
+        sregne text;
+        BEGIN
+            if NEW.regne IS NULL THEN
+                FOR sregne IN
+                    SELECT DISTINCT regne
+                    FROM taxonomie.taxref t
+                    JOIN taxonomie.bib_noms n
+                    ON t.cd_nom = n.cd_nom
+                    WHERE t.regne IS NOT NULL
+                LOOP
+                    PERFORM taxonomie.fct_build_bibtaxon_attributs_view(sregne);
+                END LOOP;
+            ELSE
+                PERFORM taxonomie.fct_build_bibtaxon_attributs_view(NEW.regne);
+            END IF;
+        RETURN NEW;
+        END
+        $$  LANGUAGE plpgsql;
+
+
+        CREATE TRIGGER trg_refresh_attributes_views_per_kingdom
+            AFTER INSERT OR UPDATE OR DELETE ON taxonomie.bib_attributs
+            FOR EACH ROW EXECUTE PROCEDURE taxonomie.trg_fct_refresh_attributesviews_per_kingdom();
+        """
+    )
--- apptax/taxonomie/routestmedias.py
+++ apptax/taxonomie/routestmedias.py
@@ -1,114 +1,42 @@
 # coding: utf8
 import logging
+from pathlib import Path
 import os
-from flask import jsonify, json, Blueprint, request, Response, g, current_app, send_file
+from flask import json, Blueprint, request, current_app, send_file
 
-from sqlalchemy.exc import IntegrityError
-from PIL import Image
 
-from pypnusershub import routes as fnauth
+from .models import TMedias
+from .schemas import TMediasSchema
 
-from . import db
-from . import filemanager
 from .filemanager import FILEMANAGER
-from ..log import logmanager
-from ..utils.utilssqlalchemy import json_resp
-from .models import TMedias, BibTypesMedia
-from .repositories import MediaRepository
+
 
 adresses = Blueprint("t_media", __name__)
 logger = logging.getLogger()
 
-media_repo = MediaRepository(db.session, current_app.config.get("S3_BUCKET_NAME"))
-
 
 @adresses.route("/", methods=["GET"])
 @adresses.route("/<int:id>", methods=["GET"])
-@json_resp
 def get_tmedias(id=None):
+    """
+    Liste des médias
+    TODO add pagination
+    """
     if id:
-        return media_repo.get_and_format_one_media(id)
-    return media_repo.get_and_format_media_filter_by(
-        filters={}, force_path=request.args.get("forcePath", False)
-    )
+        media = TMedias.query.get(id)
+        return TMediasSchema().dump(media)
+    medias = TMedias.query.all()
+    return TMediasSchema().dump(medias, many=True)
 
 
-@adresses.route("/bycdref/<int(signed=True):cdref>", methods=["GET"])
-@json_resp
-def get_tmediasbyTaxon(cdref):
-    filters = {}
-    if cdref:
-        filters = {"cd_ref": cdref}
-    obj = media_repo.get_and_format_media_filter_by(
-        filters=filters, force_path=request.args.get("forcePath", False)
-    )
-    return obj
-
-
-# REM : Route utilisée ?????
-#  Rentre en conflit avec  get_tmedias
-@adresses.route("/<type>", methods=["GET"])
-@json_resp
-def get_tmediasbyType(type):
-    filters = {}
-    if type:
-        filters = {"id_type": type}
-    obj = media_repo.get_media_filter_by(
-        filters=filters, force_path=request.args.get("forcePath", False)
-    )
-    return obj
-
-
-@adresses.route("/", methods=["POST", "PUT"])
-@adresses.route("/<int:id_media>", methods=["POST", "PUT"])
-@fnauth.check_auth(2)
-def insertUpdate_tmedias(id_media=None):
-    # récupération des données du formulaire
-    upload_file = None
-    if request.files:
-        upload_file = request.files["file"]
-
-    data = {}
-    if request.form:
-        data = request.form.to_dict()
-    else:
-        data = request.get_json(silent=True)
-
-    # Enregistrement des données du média
-    myMedia, action = media_repo.import_media(data, upload_file, id_media)
-
-    # Log
-    logmanager.log_action(
-        "bib_media",
-        myMedia.id_media,
-        repr(myMedia),
-        action,
-        "Traitement média : " + myMedia.titre,
-    )
-    return (
-        json.dumps({"success": True, "id_media": myMedia.id_media, "media": myMedia.as_dict()}),
-        200,
-        {"ContentType": "application/json"},
-    )
-
-
-@adresses.route("/<int:id_media>", methods=["DELETE"])
-@fnauth.check_auth(2)
-def delete_tmedias(id_media):
-    myMedia = media_repo.delete(id_media)
-    # Log
-    logmanager.log_action(
-        "bib_media",
-        id_media,
-        repr(myMedia),
-        "DELETE",
-        "Suppression du média : " + myMedia.titre,
-    )
-    return (
-        json.dumps({"success": True, "id_media": id_media}),
-        200,
-        {"ContentType": "application/json"},
-    )
+@adresses.route("/bycdref/<cd_ref>", methods=["GET"])
+def get_tmediasbyTaxon(cd_ref):
+    """
+    Liste des médias associés à un taxon
+    """
+    q = TMedias.query.filter_by(**{"cd_ref": cd_ref})
+    medias = q.all()
+    return TMediasSchema().dump(medias, many=True)
 
 
 @adresses.route("/thumbnail/<int:id_media>", methods=["GET"])
@@ -128,8 +56,8 @@ def getThumbnail_tmedias(id_media):
         Image générée
     """
 
-    myMedia = media_repo.get_one_media(id_media)
-    if myMedia is None:
+    media = TMedias.query.get(id_media)
+    if media is None:
         return (
             json.dumps(
                 {
@@ -143,15 +71,18 @@ def getThumbnail_tmedias(id_media):
         )
 
     params = request.args
-    pad = True
     size = (300, 400)
     if ("h" in params) or ("w" in params):
         size = (int(params.get("h", -1)), int(params.get("w", -1)))
 
+    force = False
+    if ("force" in params) and (params.get("force") == "true"):
+        force = True
     regenerate = False
     if ("regenerate" in params) and (params.get("regenerate") == "true"):
         regenerate = True
 
-    thumbpath = FILEMANAGER.create_thumb(myMedia, size, regenerate)
-
-    return send_file(thumbpath, mimetype="image/jpg")
+    thumbpath = FILEMANAGER.create_thumb(media, size, force, regenerate)
+    return send_file(
+        os.path.join(Path(current_app.config["MEDIA_FOLDER"]).absolute(), "taxhub", thumbpath)
+    )
