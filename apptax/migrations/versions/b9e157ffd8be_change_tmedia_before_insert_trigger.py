5443c3ed0e48c8fa31edef523da80817042463e2
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-11-22 18:44:13 +0100

    Feat taxhub v2 : modélisation suppression de la table bib_noms

---
+++ apptax/migrations/versions/b9e157ffd8be_change_tmedia_before_insert_trigger.py
@@ -0,0 +1,53 @@
+"""Change tmedia before insert trigger
+
+Revision ID: b9e157ffd8be
+Revises: 73306d6d64c7
+Create Date: 2022-11-22 17:41:07.543733
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9e157ffd8be'
+down_revision = '73306d6d64c7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("""
+    CREATE OR REPLACE FUNCTION taxonomie.insert_t_medias()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+    DECLARE
+        trimtitre text;
+    BEGIN
+        new.date_media = now();
+        new.cd_ref = taxonomie.find_cdref(new.cd_ref);
+        trimtitre = replace(new.titre, ' ', '');
+        RETURN NEW;
+    END;
+    $function$
+    ;
+    """)
+
+
+def downgrade():
+        op.execute("""
+    CREATE OR REPLACE FUNCTION taxonomie.insert_t_medias()
+    RETURNS trigger
+    LANGUAGE plpgsql
+    AS $function$
+    DECLARE
+        trimtitre text;
+    BEGIN
+        new.date_media = now();
+        trimtitre = replace(new.titre, ' ', '');
+        RETURN NEW;
+    END;
+    $function$
+    ;
+    """)
