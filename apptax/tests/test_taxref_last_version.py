ac784c283ce17692ef998f89c4d688c240a5be1c
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-11-25 14:55:39 +0100

    Test taxref populate

---
+++ apptax/tests/test_taxref_last_version.py
@@ -0,0 +1,16 @@
+import pytest
+
+from apptax.taxonomie.models import Taxref, TaxrefBdcStatutText
+
+
+@pytest.mark.usefixtures("client_class", "temporary_transaction")
+class TestPopulateTaxref:
+    """Test if taxref data are correctly populated"""
+
+    def test_count_taxref(self):
+        nb_taxref = Taxref.query.count()
+        assert nb_taxref == 657609
+
+    def test_count_bdc_status(self):
+        nb_bdc_texts = TaxrefBdcStatutText.query.count()
+        assert nb_bdc_texts == 873
