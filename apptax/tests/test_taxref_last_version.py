c8b4f7ef7807574ba7ead0be76d7583441267b0d
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-01-20 17:27:38 +0100

    Change nb of data assertion taxref

--- apptax/tests/test_taxref_last_version.py
+++ apptax/tests/test_taxref_last_version.py
@@ -14,7 +14,7 @@ class TestPopulateTaxref:
 
     def test_count_taxref(self):
         nb_taxref = Taxref.query.count()
-        assert nb_taxref == 672923
+        assert nb_taxref == 670946
 
     def test_count_bdc_status(self):
         nb_bdc_texts = TaxrefBdcStatutText.query.count()
