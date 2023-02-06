b091c989c8495a4b60766604990d675765de8d44
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-02-06 22:19:22 +0100

    Black

--- apptax/admin/utils.py
+++ apptax/admin/utils.py
@@ -2,6 +2,7 @@ from werkzeug.utils import secure_filename
 
 from pypnusershub.db.models import UserApplicationRight
 
+
 def taxref_media_file_name(obj, file_data):
     """
     Generate file name
@@ -12,7 +13,5 @@ def taxref_media_file_name(obj, file_data):
 def get_user_permission(id_app, id_role):
     query = UserApplicationRight.query.filter(
         UserApplicationRight.id_application == id_app
-    ).filter(
-        UserApplicationRight.id_role == id_role
-    )
+    ).filter(UserApplicationRight.id_role == id_role)
     return query.scalar()
