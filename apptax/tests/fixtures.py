517588dc13f8660d35afc44506e2e64702c1402e
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-03-16 16:37:26 +0100

    Black

--- apptax/tests/fixtures.py
+++ apptax/tests/fixtures.py
@@ -1,7 +1,7 @@
 import pytest
 
 from apptax.database import db
-from apptax.taxonomie.models import BibListes,   BibThemes, BibAttributs, CorTaxonAttribut
+from apptax.taxonomie.models import BibListes, BibThemes, BibAttributs, CorTaxonAttribut
 , Taxref
 
 bibnom_exemple = [
@@ -22,9 +22,7 @@ def noms_example():
     liste = BibListes.query.filter_by(code_liste="100").one()
     with db.session.begin_nested():
         for cd_nom, cd_ref, nom_francais, comments in bibnom_exemple:
-            nom = Taxref.query.get(
-               cd_nom
-            )
+            nom = Taxref.query.get(cd_nom)
             db.session.add(nom)
             liste.noms.append(nom)
 
