757fcbb5e0f46596e753b0d53a47e21a26b33d6a
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-03-23 10:53:56 +0100

    use CURRENT_APP parameters

--- apptax/admin/utils.py
+++ apptax/admin/utils.py
@@ -1,6 +1,7 @@
 from werkzeug.utils import secure_filename
 
 from pypnusershub.db.models import AppUser
+from pypnusershub.utils import get_current_app_id
 
 
 def taxref_media_file_name(obj, file_data):
@@ -10,7 +11,8 @@ def taxref_media_file_name(obj, file_data):
     return secure_filename(f"{obj.taxon.cd_ref}_{file_data.filename}")
 
 
-def get_user_permission(id_app, id_role):
+def get_user_permission(id_role):
+    id_app = get_current_app_id()
     query = AppUser.query.filter(AppUser.id_application == id_app).filter(
         AppUser.id_role == id_role
     )
