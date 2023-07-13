f6513a74d279c7e1d9c7728f895f73d05e6fe7df
joel.clement <joelclems@gmail.com>
2023-07-13 12:40:25 +0200

    up healthcheck

--- docker_healthcheck.sh
+++ docker_healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # docker healthcheck TH
-url_test=http://localhost:5000/taxhub/
+url_test=http://localhost:5000${TAXHUB_APPLICATION_ROOT}/
 if [ ! -f /tmp/container_healthy ]; then
     curl -f "${url_test}" || exit 1
     touch /tmp/container_healthy
