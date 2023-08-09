f1f56c69f6a5da040ba35fecdd1583124f3263c5
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-08-09 17:09:12 +0200

    set url from url_for with config

--- apptax/admin/static/js/regne_group2_inpn.js
+++ apptax/admin/static/js/regne_group2_inpn.js
@@ -1,34 +1,30 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
   // Get value of regne and group_inpn
-  let regne_group2 = {}
+  let regne_group2 = {};
 
-  let url = (APPLICATION_ROOT + "/api/taxref/regnewithgroupe2").replace("//", "/");
+  fetch(URL_GROUP_REGNE)
+    .then((response) => response.json())
+    .then((data) => {
+      regne_group2 = data;
 
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    regne_group2 = data;
-
-    // Populate value of regne
-    let $dropdown = $("#regne");
-    $('#regne').empty();
-    $dropdown.append($("<option />").val("").text("---"));
-    $.each(regne_group2, function(key, value) {
-      $dropdown.append($("<option />").val(key).text(key));
+      // Populate value of regne
+      let $dropdown = $("#regne");
+      $("#regne").empty();
+      $dropdown.append($("<option />").val("").text("---"));
+      $.each(regne_group2, function (key, value) {
+        $dropdown.append($("<option />").val(key).text(key));
+      });
     });
 
-  });
-
-  $('#regne').on('change', function() {
+  $("#regne").on("change", function () {
     let $dropdown = $("#group2_inpn");
 
     // Clear group2_inpn
-    $('#group2_inpn').empty();
+    $("#group2_inpn").empty();
     $("#group2_inpn").val("");
 
     // Populate with regne selected value
-    $.each(regne_group2[$('#regne').val()], function() {
+    $.each(regne_group2[$("#regne").val()], function () {
       $dropdown.append($("<option />").val(this).text(this));
     });
   });
