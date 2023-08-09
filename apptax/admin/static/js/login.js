a7ac11b3c03c76f151e643c86674ab2eeb72bdd9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-08-09 17:09:12 +0200

    set url from url_for with config

--- apptax/admin/static/js/login.js
+++ apptax/admin/static/js/login.js
@@ -1,37 +1,33 @@
-
-$(document).ready(function() {
-  $("#login-submit").click(function() {
+$(document).ready(function () {
+  $("#login-submit").click(function () {
     const login_data = {
-      login:  $("#identifiant").val(),
+      login: $("#identifiant").val(),
       password: $("#password").val(),
-      id_application:  $("#id-app").val()
+      id_application: $("#id-app").val(),
     };
 
-    let url = (APPLICATION_ROOT + "/api/auth/login").replace("//", "/");
-
-    fetch(url, {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body:  JSON.stringify(login_data)
-      }).then(
-        (response) => {
-          $('#login-error').hide();
-          if (!response.ok) {
-            return Promise.reject(response.json());
-          }
-
-          return response.json()
+    fetch(URL_LOGIN, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(login_data),
+    })
+      .then((response) => {
+        $("#login-error").hide();
+        if (!response.ok) {
+          return Promise.reject(response.json());
         }
-      )
-      .then((data) => {
-        location.href = $("#return-url").val()
+
+        return response.json();
       })
-      .catch(error => {
-          console.error('There was an error!', error);
-          $('#login-error').show();
+      .then((data) => {
+        location.href = $("#return-url").val();
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        $("#login-error").show();
       });
-  })
+  });
 });
