1985fb759834572b1f38b30a5a843c03cca56b23
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-08-06 16:22:05 +0200

    update Taxhuv Attributs forms (validation on given values, label + formaters+ descriptions,

--- apptax/admin/forms.py
+++ apptax/admin/forms.py
@@ -1,4 +1,72 @@
+import logging
+import json
+
+from flask import request, flash
+
 from flask_admin import form
+from flask_admin.form import BaseForm
+from marshmallow import Schema, ValidationError, fields
+
+log = logging.getLogger(__name__)
+
+
+def generate_schema_additional_attribut(type_: str = None):
+    """
+    Generate a schema for additional attributes.
+
+    Parameters
+    ----------
+    type_ : str, optional
+        The type of the additional attributes. Can be "int", "json" or None.
+        If None, the default type is "str".
+
+    Returns
+    -------
+    class
+        A schema for additional attributes.
+
+    """
+    if type_ == "int":
+        type_ = fields.Integer()
+    elif type_ == "json":
+        type_ = fields.Dict()
+    else:
+        type_ = fields.String()
+
+    class AdditionalAttributSchema(Schema):
+        values = fields.List(type_, load_default=[])
+
+    return AdditionalAttributSchema
+
+
+class TAdditionalAttributForm(BaseForm):
+    """
+    Override BaseForm to include the validation of the values list during the creation/edition of an attribut in TaxHub
+    """
+
+    def validate(self, extra_validators=None):
+        if request.endpoint in ["bibattributs.edit_view", "bibattributs.create_view"]:
+            try:
+                data = json.loads(self.data["liste_valeur_attribut"])
+            except json.JSONDecodeError as e:
+                log.exception("Liste valeur attribut must be a JSON string")
+                flash("'Valeurs disponibles' doit être au format JSON", "error")
+                return False
+            try:
+                type_ = self.data["type_attribut"]
+                generate_schema_additional_attribut(type_)().load(data)
+            except ValidationError as e:
+                log.exception(
+                    """JSON doesn't match wanted format {'values':[val1,val2,...]} or "values" """
+                    """contains a value that does not match te type_attirbut declared"""
+                )
+                flash(
+                    """'Valeurs disponibles' doit être format {"values":[val1,val2,...]}"""
+                    """ et les valeurs doivent respecter le type déclaré dans 'Type Attribut'""",
+                    "error",
+                )
+                return False
+        return super().validate(extra_validators)
 
 
 class ImageUploadFieldWithoutDelete(form.ImageUploadField):
