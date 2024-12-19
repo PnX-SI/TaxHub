d4ffae61fe305c3bfc5386d8e08526cdb75874c9
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-12-19 10:40:28 +0100

    fix lint

--- apptax/migrations/versions/2c68a907f74c_increase_t_medias_source_size.py
+++ apptax/migrations/versions/2c68a907f74c_increase_t_medias_source_size.py
@@ -5,24 +5,29 @@ Revises: 3c4762751898
 Create Date: 2024-12-19 10:31:05.778720
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2c68a907f74c'
-down_revision = '3c4762751898'
+revision = "2c68a907f74c"
+down_revision = "3c4762751898"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.alter_column('t_medias', 'source',
-        type_=sa.Unicode(),
-        existing_nullable=True,schema='taxonomie')
+    op.alter_column(
+        "t_medias", "source", type_=sa.Unicode(), existing_nullable=True, schema="taxonomie"
+    )
 
 
 def downgrade():
-    op.alter_column('t_medias', 'source',
+    op.alter_column(
+        "t_medias",
+        "source",
         type_=sa.VARCHAR(length=25),
-        existing_nullable=True,schema='taxonomie')
+        existing_nullable=True,
+        schema="taxonomie",
+    )
