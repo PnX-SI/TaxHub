5618fb92f9ffde188d906976b72457b8352f5035
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-01-16 12:07:25 +0100

    Taxref v18 : Import + Migrate

--- apptax/taxonomie/commands/migrate_taxref/test_commands_migrate.py
+++ apptax/taxonomie/commands/migrate_taxref/test_commands_migrate.py
@@ -3,6 +3,7 @@ import click
 from click.testing import CliRunner
 from flask.cli import with_appcontext
 
+from sqlalchemy import select, delete, func
 from sqlalchemy.orm.exc import NoResultFound
 
 from apptax.database import db
@@ -60,11 +61,37 @@ data_migration_taxref_v16_to_v17 = [
     (713870, 54376, 713870, "Leptidea sinapis sinapis (Linnaeus, 1758)", None),  # split
 ]
 
+# test migration taxref 17 vers 18 détection des différents cas
+# cd_nom, cd_ref, new_cd_ref, nom_complet, attr_value
+data_migration_taxref_v17_to_v18 = [
+    (997791, 997791, None, "Poropila dubia J.Schiller, 1925", None),  # cd_nom sans substition
+    (608162, 608162, 104384, "Juncus x langei Erdner, 1906", None),  # cd_nom avec substition
+    (54502, 54502, 1042429, "Parnassius mnemosyne (Linnaeus, 1758)", "A"),  # update cd_ref
+    (103749, 103749, 103749, "Iris lutescens Lam., 1789", "A"),  # merge
+    (136857, 136857, 103749, "Iris lutescens subsp. lutescens Lam., 1789", "A"),  # merge
+    (116456, 116456, 116456, "Pulsatilla rubra (Lam.) Delarbre, 1800", "A"),  # merge with conflict
+    (
+        150342,
+        150342,
+        116456,
+        "Pulsatilla rubra var. rubra (Lam.) Delarbre, 1800",
+        "B",
+    ),  # merge with conflict
+    (29552, 29574, 29552, "Leccinum brunneogriseolum Lannoy & Estadès, 1991", "A"),  # split
+    (
+        29553,
+        29574,
+        29574,
+        "Leccinum brunneogriseolum f. chlorinum Lannoy & Estadès, 1993",
+        None,
+    ),  # split
+]
+
 
 def populate_data(sample_data):
-    liste = BibListes.query.filter_by(code_liste="100").one()
+    liste = db.session.scalar(select(BibListes).where(BibListes.code_liste == "100"))
+    theme = db.session.scalar(select(BibThemes).where(BibThemes.nom_theme == "Mon territoire"))
 
-    theme = BibThemes.query.filter_by(nom_theme="Mon territoire").one()
     attribut = BibAttributs(
         nom_attribut="test",
         label_attribut="Test",
@@ -81,9 +108,9 @@ def populate_data(sample_data):
 
     for cd_nom, cd_ref, new_cd_ref, nom_complet, attr_value in sample_data:
         # cor_nom_liste
-        nom = Taxref.query.get(cd_nom)
+        nom = db.session.scalar(select(Taxref).where(Taxref.cd_nom == cd_nom))
+        nom.listes.append(liste)
         db.session.add(nom)
-        liste.noms.append(nom)
 
         # medias
         media = TMedias(
@@ -110,21 +137,16 @@ def clean_data(sample_data):
         if nom:
             nom.listes = []
             db.session.add(nom)
-        res = CorTaxonAttribut.query.filter(CorTaxonAttribut.cd_ref == new_cd_ref).all()
-        for c in res:
-            db.session.delete(c)
-        res = TMedias.query.filter(TMedias.cd_ref == new_cd_ref).all()
-        for c in res:
-            db.session.delete(c)
+        db.session.execute(delete(CorTaxonAttribut).where(CorTaxonAttribut.cd_ref == new_cd_ref))
+        db.session.execute(delete(CorTaxonAttribut).where(CorTaxonAttribut.cd_ref == cd_ref))
+        db.session.execute(delete(cor_nom_liste).where(cor_nom_liste.c.cd_nom == cd_nom))
+        db.session.execute(delete(TMedias).where(TMedias.cd_ref == new_cd_ref))
+        db.session.execute(delete(TMedias).where(TMedias.cd_ref == cd_ref))
 
-    try:
-        for c in res:
-            db.session.delete(c)
-        attr = BibAttributs.query.filter(BibAttributs.nom_attribut == "test").one()
+    # Suppression attribut
+    db.session.execute(delete(BibAttributs).where(BibAttributs.nom_attribut == "test"))
 
-        db.session.delete(attr)
-    except NoResultFound:
-        pass
+    # Commit
     db.session.commit()
 
 
@@ -331,6 +353,103 @@ def test_import_taxref_v17():
     assert results == 5
 
 
+def test_import_taxref_v18():
+    from apptax.taxonomie.commands.migrate_taxref.commands_v18 import (
+        import_taxref_v18,
+        test_changes_detection,
+        apply_changes,
+    )
+
+    """Test des commandes de migration de taxref v16 vers taxref v18
+
+    Etapes :
+      - données de test migration_example :
+        - Erreur : merge de taxon avec attributs contradictoire
+        - Erreur : cd_nom disparu sans cd_nom de remplacement
+      - import de taxref v18
+      - correction des erreurs
+      - migration des données
+      - vérification des modifications réalisées lors de l'import
+    """
+    runner = CliRunner()
+    runner.invoke(import_taxref_v18, [])
+
+    # Test generated files
+    data = open_csv_file("liste_changements.csv")
+    # Test 2 conflicts
+    conflict = [d for d in data if d["action"] == "Conflicts with attributes : test: A, test: B"]
+    assert len(conflict) == 2
+
+    # Test 4 merge
+    merge = [d for d in data if d["cas"] == "merge"]
+    assert len(merge) == 4
+    # Test 2 update cd_ref
+    update_cd_ref = [d for d in data if d["cas"] == "update cd_ref"]
+    assert len(update_cd_ref) == 1
+
+    # Résolution des conflits : Erreur liée à la fusion des noms Pulsatilla rubra
+    # (150342, 150342, 116456, "Pulsatilla rubra var. rubra (Lam.) Delarbre, 1800", "B"), # merge with conflict
+    db.session.execute(delete(CorTaxonAttribut).where(CorTaxonAttribut.cd_ref == 150342))
+    db.session.commit()
+
+    runner.invoke(test_changes_detection, [])
+    data = open_csv_file("liste_changements.csv")
+    # Test plus de conflits
+    conflict = [d for d in data if d["action"] == "Conflicts with attributes : test: A, test: B"]
+    assert len(conflict) == 0
+
+    # test nom avec ou sans substition
+    # Missing 2 : cor_nom_liste et t_medias
+    data = open_csv_file("missing_cd_nom_into_database.csv")
+
+    sans_substitution = [d for d in data if d["cd_nom_remplacement"] == ""]
+    assert len(sans_substitution) == 2
+    avec_substitution = [d for d in data if not d["cd_nom_remplacement"] == ""]
+    assert len(avec_substitution) == 2
+
+    # Erreur liée au taxon sans substition
+    # (997791, 997791, None, "Poropila dubia J.Schiller, 1925", None),  # cd_nom sans substition
+    tax_sans_substitution = db.session.scalar(select(Taxref).where(Taxref.cd_nom == 997791))
+    tax_sans_substitution.listes = []
+
+    db.session.execute(delete(TMedias).where(TMedias.cd_ref == 997791))
+    db.session.commit()
+
+    runner.invoke(test_changes_detection, [])
+    data = open_csv_file("missing_cd_nom_into_database.csv")
+    sans_substitution = [d for d in data if d["cd_nom_remplacement"] == ""]
+    assert len(sans_substitution) == 0
+
+    #  Migration de taxref
+    runner.invoke(apply_changes, ["--keep-oldtaxref"])
+
+    # Analyse de la migration
+    # cor_nom_liste : nb enregistrements initial = 9 ; final = 8
+    #   perte de 1 du à la suppression du cd_nom 997791
+    nb_cor_liste = db.session.scalar(select(func.count()).select_from(cor_nom_liste))
+    assert nb_cor_liste == 8
+
+    # cor_taxon_attribut : nb enregistrements initial = 6 ; final = 4
+    #  perte de 2 du au merge des taxons 103749 + 136857 et 116456 + 150342
+    nb_attr = db.session.scalar(select(func.count()).select_from(CorTaxonAttribut))
+    assert nb_attr == 4
+
+    # t_medias :
+    # nb media initial = 9 ; final = 8
+    # nb de taxon  initial = 8 ; final = 5
+    # perte de 3 taxons :
+    #       - 2 du au merge des taxons 103749 + 136857 et 116456 + 150342
+    #       - 1 du à la suppression du cd_nom 997791
+    # perte de 1 média du à la suppression sans remplacement de 997791
+    nb_media = db.session.scalar(select(func.count()).select_from(TMedias))
+    assert nb_media == 8
+
+    nb_media_taxa = db.session.scalar(
+        select(func.count(TMedias.cd_ref.distinct())).select_from(TMedias)
+    )
+    assert nb_media_taxa == 5
+
+
 @test_migrate_taxref.command()
 @with_appcontext
 def test_taxref_v16_migration():
@@ -355,3 +474,16 @@ def test_taxref_v17_migration():
         raise (e)
     finally:
         clean_data(data_migration_taxref_v16_to_v17)
+
+
+@test_migrate_taxref.command()
+@with_appcontext
+def test_taxref_v18_migration():
+    """Test des commandes de migration de taxref v17 vers taxref v18"""
+    populate_data(data_migration_taxref_v17_to_v18)
+    try:
+        test_import_taxref_v18()
+    except AssertionError as e:
+        raise (e)
+    finally:
+        clean_data(data_migration_taxref_v17_to_v18)
