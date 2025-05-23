5ed6f6ae8dcf924f11611a0908575fdab81d18d4
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-05-23 10:48:45 +0200

    fix(requirements) : add hack to  increase pillow version

--- apptax/__init__.py
+++ apptax/__init__.py
@@ -6,3 +6,9 @@ taxhub_api_routes = [
     ("apptax.taxonomie.routestmedias:adresses", "/tmedias"),
     ("apptax.taxonomie.routesbdcstatuts:adresses", "/bdc_statuts"),
 ]
+
+from PIL import Image, __version__
+from pkg_resources import parse_version
+
+if parse_version(__version__) >= parse_version("10.0.0"):
+    Image.ANTIALIAS = Image.LANCZOS
