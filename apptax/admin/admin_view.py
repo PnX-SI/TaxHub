85ed7da9a6e6b636e0a8fedd7b684af964b0f776
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-09-11 17:21:52 +0200

    Ajout fonctionnalité d'export depuis la vue taxref - cf #632

--- apptax/admin/admin_view.py
+++ apptax/admin/admin_view.py
@@ -313,9 +313,32 @@ class TaxrefView(
     ModelView,
 ):
     can_create = False
-    can_export = False
     can_delete = False
     can_view_details = True
+    # Configuration de l'export
+    can_export = True
+    export_max_rows = 10000
+    column_export_list = (
+        "regne",
+        "group1_inpn",
+        "group2_inpn",
+        "group3_inpn",
+        "classe",
+        "ordre",
+        "famille",
+        "cd_nom",
+        "cd_ref",
+        "nom_complet",
+        "nom_valide",
+        "nom_vern",
+        "rang",
+        "listes",
+        "attributs",
+        "nb_medias",
+    )
+    column_formatters_export = dict(
+        listes=lambda v, c, m, p: ",".join([l.nom_liste for l in m.listes])
+    )
 
     @property
     def can_edit(self):
