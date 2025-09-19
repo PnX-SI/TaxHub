9fcf59ef4be493cd5ed63fed5745ae2dfc57a2e6
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-09-19 10:55:54 +0200

    [pytest]Add tests for media

--- apptax/database.py
+++ apptax/database.py
@@ -3,6 +3,7 @@ from os import environ
 from importlib import import_module
 from pathlib import Path
 
+import requests
 from flask_sqlalchemy import SQLAlchemy
 
 if sys.version_info < (3, 9):
@@ -27,3 +28,12 @@ try:
 except PackageNotFoundError:
     with open(str((ROOT_DIR / "VERSION"))) as v:
         TAXHUB_VERSION = v.read()
+
+
+def generate_user_agent() -> str:
+    """Generate a generic user-agent description for requests
+    Returns:
+        str: generic user-agent
+    """
+    user_agent = f"TaxHub/{TAXHUB_VERSION} (https://github.com/PnX-SI/TaxHub) python-requests/{requests.__version__}"
+    return user_agent
