ddd6ad936efbfe087c722ee58644358309aed295
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-09-24 14:42:00 +0200

    Fix/thumbnail (#646)
    * feat(thumbnail): take in a account when both width and height are given
    
    * add a test for thumbnail generation
    
    * refact
    
    * fix test
    
    * wip

--- apptax/taxonomie/routestmedias.py
+++ apptax/taxonomie/routestmedias.py
@@ -78,7 +78,7 @@ def getThumbnail_tmedias(id_media):
                 {
                     "success": False,
                     "id_media": id_media,
-                    "message": "Le média demandé n" "éxiste pas",
+                    "message": "Le média demandé n'existe pas",
                 }
             ),
             400,
@@ -91,6 +91,8 @@ def getThumbnail_tmedias(id_media):
 
     height_params: str = params.get("h", None)
     width_params: str = params.get("w", None)
+    force = params.get("force") == "true"
+    regenerate = params.get("regenerate") == "true"
 
     if (width_params and not width_params.isdigit()) or (
         height_params and not height_params.isdigit()
@@ -103,12 +105,9 @@ def getThumbnail_tmedias(id_media):
     if height_params and not width_params:
         size = [-1, int(height_params)]
 
-    force = False
-    if ("force" in params) and (params.get("force") == "true"):
+    if height_params and width_params:
+        size = (int(width_params), int(height_params))
         force = True
-    regenerate = False
-    if ("regenerate" in params) and (params.get("regenerate") == "true"):
-        regenerate = True
 
     thumbpath = FILEMANAGER.create_thumb(media, size, force, regenerate)
     if thumbpath:
