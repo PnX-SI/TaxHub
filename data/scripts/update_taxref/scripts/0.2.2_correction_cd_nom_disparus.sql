280ff7c536336d75be504155ac2f5c92853ce662
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2026-02-06 15:07:59 +0100

    Suppression des scripts de mise à jour de taxref obsoletes

--- data/scripts/update_taxref/scripts/0.2.2_correction_cd_nom_disparus.sql
+++
@@ -1,9 +0,0 @@
-﻿
--- Export des données supprimées
--- Attention l'utilisateur qui exécute ce script doit être superuser
-COPY (
-	SELECT *
-	FROM taxonomie.bib_noms
-	WHERE  deleted = true
-) TO '/tmp/liste_cd_nom_disparus_bib_noms.csv'
-DELIMITER ';' CSV HEADER;
