32ab5a14d8cf2eb960d3a3a770b10c1dd25d6e5c
Amandine <amandine.sahl@cevennes-parcnational.fr>
2026-03-12 14:18:11 +0100

    Fix lien vers taxref depuis bib liste (#682)
    * Fix lien vers taxref depuis bib liste
    * Fix : nb of results for list filter

--- apptax/admin/filters.py
+++ apptax/admin/filters.py
@@ -57,7 +57,7 @@ class FilterTaxrefAttr(DynamicOptionsMixin, FilterEqual):
 
 class FilterBiblist(DynamicOptionsMixin, FilterEqual):
     def apply(self, query, value, alias=None):
-        return query.join(Taxref.listes).filter(Taxref.listes.any(id_liste=value))
+        return query.filter(Taxref.listes.any(id_liste=value))
 
     def get_dynamic_options(self, view):
         if has_app_context():
